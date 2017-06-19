The-Legion-Preservation-Project/AshamaneCore

Core/Grids: Ported cmangos/mangos-wotlk@ea99457e50790acde8928aa0f3dc4a9c096b4a8c

src/server/game/Grids/Cells/CellImpl.h
@@ -61,7 +61,15 @@ inline CellArea Cell::CalculateCellArea(float x, float y, float radius)
 }
 
 template<class T, class CONTAINER>
-inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, float radius, float x_off, float y_off) const
+inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, WorldObject const& obj, float radius) const
+{
+    //we should increase search radius by object's radius, otherwise
+    //we could have problems with huge creatures, which won't attack nearest players etc
+    Visit(standing_cell, visitor, map, obj.GetPositionX(), obj.GetPositionY(), radius + obj.GetObjectSize());
+}
+
+template<class T, class CONTAINER>
+inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, float x_off, float y_off, float radius) const
 {
     if (!standing_cell.IsCoordValid())
         return;
@@ -119,14 +127,6 @@ inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, 
 }
 
 template<class T, class CONTAINER>
-inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, WorldObject const& obj, float radius) const
-{
-    //we should increase search radius by object's radius, otherwise
-    //we could have problems with huge creatures, which won't attack nearest players etc
-    Visit(standing_cell, visitor, map, radius + obj.GetObjectSize(), obj.GetPositionX(), obj.GetPositionY());
-}
-
-template<class T, class CONTAINER>
 inline void Cell::VisitCircle(TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, CellCoord const& begin_cell, CellCoord const& end_cell) const
 {
     //here is an algorithm for 'filling' circum-squared octagon
@@ -177,4 +177,81 @@ inline void Cell::VisitCircle(TypeContainerVisitor<T, CONTAINER>& visitor, Map& 
         }
     }
 }
+
+template<class T>
+inline void Cell::VisitGridObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitWorldObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitAllObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *center_obj->GetMap(), *center_obj, radius);
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitGridObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *map, x, y, radius);
+}
+
+template<class T>
+inline void Cell::VisitWorldObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *map, x, y, radius);
+}
+
+template<class T>
+inline void Cell::VisitAllObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *map, x, y, radius);
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *map, x, y, radius);
+}
+
 #endif

gitdnd/TrinityCore

(cherry picked from commit 9299e9bde087af7e5d777d5a55a3b79d2b63c48e)

src/server/game/Grids/Cells/CellImpl.h
@@ -61,7 +61,15 @@ inline CellArea Cell::CalculateCellArea(float x, float y, float radius)
 }
 
 template<class T, class CONTAINER>
-inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, float radius, float x_off, float y_off) const
+inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, WorldObject const& obj, float radius) const
+{
+    //we should increase search radius by object's radius, otherwise
+    //we could have problems with huge creatures, which won't attack nearest players etc
+    Visit(standing_cell, visitor, map, obj.GetPositionX(), obj.GetPositionY(), radius + obj.GetCombatReach());
+}
+
+template<class T, class CONTAINER>
+inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, float x_off, float y_off, float radius) const
 {
     if (!standing_cell.IsCoordValid())
         return;
@@ -119,14 +127,6 @@ inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, 
 }
 
 template<class T, class CONTAINER>
-inline void Cell::Visit(CellCoord const& standing_cell, TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, WorldObject const& obj, float radius) const
-{
-    //we should increase search radius by object's radius, otherwise
-    //we could have problems with huge creatures, which won't attack nearest players etc
-    Visit(standing_cell, visitor, map, radius + obj.GetCombatReach(), obj.GetPositionX(), obj.GetPositionY());
-}
-
-template<class T, class CONTAINER>
 inline void Cell::VisitCircle(TypeContainerVisitor<T, CONTAINER>& visitor, Map& map, CellCoord const& begin_cell, CellCoord const& end_cell) const
 {
     //here is an algorithm for 'filling' circum-squared octagon
@@ -177,5 +177,82 @@ inline void Cell::VisitCircle(TypeContainerVisitor<T, CONTAINER>& visitor, Map& 
         }
     }
 }
+
+template<class T>
+inline void Cell::VisitGridObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitWorldObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitAllObjects(WorldObject const* center_obj, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(center_obj->GetPositionX(), center_obj->GetPositionY()));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *center_obj->GetMap(), *center_obj, radius);
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *center_obj->GetMap(), *center_obj, radius);
+}
+
+template<class T>
+inline void Cell::VisitGridObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *map, x, y, radius);
+}
+
+template<class T>
+inline void Cell::VisitWorldObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *map, x, y, radius);
+}
+
+template<class T>
+inline void Cell::VisitAllObjects(float x, float y, Map* map, T& visitor, float radius, bool dont_load /*= true*/)
+{
+    CellCoord p(Trinity::ComputeCellCoord(x, y));
+    Cell cell(p);
+    if (dont_load)
+        cell.SetNoCreate();
+
+    TypeContainerVisitor<T, WorldTypeMapContainer> wnotifier(visitor);
+    cell.Visit(p, wnotifier, *map, x, y, radius);
+    TypeContainerVisitor<T, GridTypeMapContainer> gnotifier(visitor);
+    cell.Visit(p, gnotifier, *map, x, y, radius);
+}
+
 #endif
 