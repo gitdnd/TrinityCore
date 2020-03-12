jairoe05/tc_duplicate

Core/PacketIO: Updated and enabled CMSG_WORLD_TELEPORT (GM opcode)

src/server/game/Server/Packets/MiscPackets.cpp
@@ -545,3 +545,11 @@ void WorldPackets::Misc::SetPvP::Read()
 {
     EnablePVP = _worldPacket.ReadBit();
 }
+
+void WorldPackets::Misc::WorldTeleport::Read()
+{
+    _worldPacket >> MapID;
+    _worldPacket >> TransportGUID;
+    _worldPacket >> Pos;
+    _worldPacket >> Facing;
+}

gitdnd/TrinityCore

(cherry picked from commit 7632e1182584236ab2e8069ac6e3739fd7de47b0)

src/server/game/Server/Packets/MiscPackets.cpp
@@ -115,3 +115,10 @@ WorldPacket const* WorldPackets::Misc::OverrideLight::Write()
     return &_worldPacket;
 }
 
+void WorldPackets::Misc::WorldTeleport::Read()
+{
+    _worldPacket >> Time;
+    _worldPacket >> MapID;
+    _worldPacket >> Pos;
+    _worldPacket >> Facing;
+}