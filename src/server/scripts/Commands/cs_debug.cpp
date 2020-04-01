405f11f92b7c25134b1965b97292b9f970d279df
Terrorblade <tbgitemail@gmail.com>
2020-04-01 04:36:50 -0400

    Missing ;

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -1969,7 +1969,7 @@ public:
         if (dynObj->CreateDynamicObject(player->GetMap()->GenerateLowGuid<HighGuid::DynamicObject>(), player, spell, player->GetPosition(), radius, DynamicObjectType(type)))
             dynObj->SetDuration(duration);
         else
-            handler->PSendSysMessage("Dyn Obj spawn failed.")
+            handler->PSendSysMessage("Dyn Obj spawn failed.");
         return true;
     }
 };
