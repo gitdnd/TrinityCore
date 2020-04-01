b5214c568fecf6d017f6de45ccd8c569d65a95d4
Terrorblade <tbgitemail@gmail.com>
2020-04-01 19:39:11 -0400

    I'm a retard that has been forgetting the existence of ().

--- src/server/game/LuaEngine/LuaEngine.cpp
+++ src/server/game/LuaEngine/LuaEngine.cpp
@@ -223,7 +223,7 @@ void Eluna::OpenLua()
     }
     std::ostringstream command;
     command << "cd " << std::experimental::filesystem::current_path() << "\lua_scripts" << " & git pull";
-    system(command.str);
+    system(command.str().c_str());
 
     L = luaL_newstate();
 
