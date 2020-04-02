fc00ee9b3b9ff9a5a22b465d76baf758f468f0d6
Oliver M <oliver.m.m@hotmail.com>
2020-04-02 23:32:20 +0200

    Added GOMove from Rochet2

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,9 +17,12 @@
 
 // This is where scripts' loading functions should be declared:
 
+//GOMove Addon
+void AddSC_GOMove_commandscript();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
+    AddSC_GOMove_commandscript();
 }
