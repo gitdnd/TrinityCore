fa166b888f025538d0862542c6fd878de76796d6
Server <server@server.com>
2020-04-15 21:05:37 +0200

    Fix typo

--- src/server/game/Groups/Group.h
+++ src/server/game/Groups/Group.h
@@ -269,7 +269,7 @@ class TC_GAME_API Group
         void SetDungeonDifficulty(Difficulty difficulty);
         void SetRaidDifficulty(Difficulty difficulty);
         void SetDungeonLevel(int dungeonLevel);
-        int GetDungeonLevel() { return m_dungeonLevel };
+        int GetDungeonLevel() { return m_dungeonLevel; }
         uint16 InInstance();
         bool InCombatToInstance(uint32 instanceId);
         void ResetInstances(uint8 method, bool isRaid, Player* SendMsgTo);
