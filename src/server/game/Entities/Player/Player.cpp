d05ab207b2cfd2dd5969e21a0b84cf0ac965731b
Server <server@server.com>
2020-05-09 15:49:52 +0200

    More debug messages

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -8460,6 +8460,9 @@ void Player::SendLoot(ObjectGuid guid, LootType loot_type)
                     return;
                 }
 
+            // Debug messsages
+            Say("Player.cpp filling chest loot", (Language)0, this);
+
             if (lootid)
             {
                 loot->clear();
