8fdcf8d9e35ec8cd66ff313f35ce066df05843c5
Server <server@server.com>
2020-05-10 15:56:51 +0200

    Use transport ID in Lua method

--- src/server/game/LuaEngine/WorldObjectMethods.h
+++ src/server/game/LuaEngine/WorldObjectMethods.h
@@ -1204,7 +1204,7 @@ namespace LuaWorldObject
     int CreateTransport(lua_State* L, WorldObject* obj)
     {
         uint32 objectId = Eluna::CHECKVAL<uint32>(L, 2);
-        Transport* transport = TransportMgr::instance()->CreateTransport(188511, 0, obj->GetMap());
+        Transport* transport = TransportMgr::instance()->CreateTransport(objectId, 0, obj->GetMap());
         if (transport && obj->ToPlayer())
         {
             obj->ToPlayer()->Say("Successfully created transport", (Language)0, obj->ToPlayer());
