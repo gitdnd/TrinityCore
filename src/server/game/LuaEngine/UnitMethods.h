91c3327ede834eb3d4caabe0e260edea5f9ef3d2
Terrorblade <tbgitemail@gmail.com>
2020-05-14 17:27:01 -0400

    Fucking lua engine weirdness.

--- src/server/game/LuaEngine/UnitMethods.h
+++ src/server/game/LuaEngine/UnitMethods.h
@@ -3065,9 +3065,9 @@ namespace LuaUnit
 
     int SendEncounterFrame(lua_State* L, Unit* unit)
     {
-        uint32 type = Eluna::CHECKVAL<uint32>(L, 0);
-        uint8 param1 = Eluna::CHECKVAL<uint8>(L, 1);
-        uint8 param2 = Eluna::CHECKVAL<uint8>(L, 2);
+        uint32 type = Eluna::CHECKVAL<uint32>(L, 2);
+        uint8 param1 = Eluna::CHECKVAL<uint8>(L, 3);
+        uint8 param2 = Eluna::CHECKVAL<uint8>(L, 4);
         if(type > 6)
             return luaL_error(L, "SendEncounterFrame type larger then expected ( > 6)");
         WorldPacket data(SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT, 15);
