69ba04f22976622dfb22003fe8210e17dab19378
Server <server@server.com>
2020-07-25 16:03:17 +0200

    Revert "Update urand to use seed argument"
    This reverts commit 146576537a97bf94848db328e523809800b6391f.

--- src/common/Utilities/Random.cpp
+++ src/common/Utilities/Random.cpp
@@ -54,9 +54,8 @@ uint32 urand(uint32 min, uint32 max)
 uint32 urand(uint32 min, uint32 max, char* seed)
 {
     ASSERT(max >= min);
-    std::default_random_engine generator(seed);
     std::uniform_int_distribution<uint32> uid(min, max);
-    return uid(generator);
+    return uid(engine);
 }
 
 uint32 urandms(uint32 min, uint32 max)
