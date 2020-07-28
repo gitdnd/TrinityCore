1618d08b92535e97ec00476908963e0c717c3d5a
Server <server@server.com>
2020-07-29 01:01:06 +0200

    Test static stat pool

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -402,6 +402,9 @@ void VirtualItemMgr::GenerateStats(VirtualItemTemplate* output, std::mt19937& ge
         pool = modifier.statpool;
     ASSERT(pool >= 0 && pool < 0x7FFF);
 
+    // debug test
+    pool = 300;
+
     // modify stat pool size depending on item quality
     pool = (pool * quality) / 2;
 
