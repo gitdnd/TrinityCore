59550c701d6400600867a74a752703f3988e910b
Foereaper <foereaper@elunatech.com>
2020-08-19 22:00:54 +0200

    More of the above

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -372,7 +372,7 @@ void VirtualItemMgr::GenerateStats(VirtualItemTemplate* output, std::mt19937& ge
     // since our stats are based on the ilevel of the item, regenerating with a new ilevel causes problems.
     // instead, we distribute the pool based on a static pool size, and use that as a percentage value
     // when distributing the actual stat values.
-    uint32 percentile_pool = 100;
+    int16 percentile_pool = 100;
 
     // select stat group
     StatGroup statgroupid = modifier.statgroup;
@@ -433,7 +433,7 @@ void VirtualItemMgr::GenerateStats(VirtualItemTemplate* output, std::mt19937& ge
             // if we are at a free stat slot or we are at a stat slot that has the same stat type
             if (j >= setStats || output->ItemStat[j].ItemStatType == selectedStats[i])
             {
-                uint32 finalStatValue = std::floor(((distributedPool[i]/100)*pool) / VirtualModifier::GetStatRate(selectedStats[i]) * VirtualModifier::GetSlotStatModifier(output));
+                uint32 finalStatValue = std::floor((pool * (distributedPool[i] / 100)) / VirtualModifier::GetStatRate(selectedStats[i]) * VirtualModifier::GetSlotStatModifier(output));
                 output->ItemStat[j].ItemStatType = selectedStats[i];
                 output->ItemStat[j].ItemStatValue += finalStatValue;
                 setStats = std::max(setStats, uint32(j + 1));
