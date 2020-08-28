9c822fa35637ad5c3a73b1da03f0f28628e4e391
Server <server@server.com>
2020-08-28 20:32:28 +0200

    Updates to DragonIsles aura scripts

--- src/server/scripts/Custom/DragonIsles.cpp
+++ src/server/scripts/Custom/DragonIsles.cpp
@@ -24,22 +24,47 @@ public:
         PrepareAuraScript(spell_vrykul_flag_AuraScript);
 
     private:
-        void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        void HandleEffectRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
         {
             // Summon Vrykul Flag
-            GetCaster()->SummonCreature(52022, GetCaster()->GetPosition(), TEMPSUMMON_MANUAL_DESPAWN, 0);
+            if (auto caster = GetCaster())
+            {
+                GetCaster()->Yell("Oops, I dropped the flag!", Language(0), GetCaster());
+                GetCaster()->SummonCreature(52022, GetCaster()->GetPosition(), TEMPSUMMON_MANUAL_DESPAWN, 0);
+            }
         }
 
         void Register() override
         {
-            OnEffectRemove += AuraEffectRemoveFn(spell_vrykul_flag_AuraScript::OnRemove, EFFECT_1, SPELL_AURA_PERIODIC_DUMMY, AURA_EFFECT_HANDLE_REAL);
+            OnEffectRemove += AuraEffectRemoveFn(spell_vrykul_flag_AuraScript::HandleEffectRemove, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
         }
     };
+
+    /*class spell_vrykul_flag_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_vrykul_flag_SpellScript);
+
+    private:
+        void OnSomething()
+        {
+
+        }
+
+        void Register() override
+        {
+            //OnEffectHitTarget += SpellEffectFn(spell_request_second_mug::HandleScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+        }
+    };*/
 
     AuraScript* GetAuraScript() const override
     {
         return new spell_vrykul_flag_AuraScript();
     }
+
+    /*SpellScript* GetSpellScript() const override
+    {
+        return new spell_vrykul_flag_SpellScript();
+    }*/
 };
 
 void AddSC_DragonIsles()
