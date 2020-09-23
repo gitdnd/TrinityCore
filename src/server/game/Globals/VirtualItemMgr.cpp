076179272786db055dc36e26170b2bd66b8f7ac5
Foereaper <foereaper@elunatech.com>
2020-09-23 20:08:15 +0200

    Normalize ranged weapon damage with ammo removal

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -399,16 +399,16 @@ void VirtualItemMgr::GenerateStats(VirtualItemTemplate* output, std::mt19937& ge
             case ITEM_SUBCLASS_WEAPON_CROSSBOW:
             {
                 output->Delay = (urand(15, 34, generator) * 100);
-                output->Damage[0].DamageMin = ((0.93f * float(ilevel)) * 0.85f) * (float(output->Delay) / 1000.0f);
-                output->Damage[0].DamageMax = ((0.93f * float(ilevel)) * 1.15f) * (float(output->Delay) / 1000.0f);
+                output->Damage[0].DamageMin = ((1.2f * float(ilevel)) * 0.85f) * (float(output->Delay) / 1000.0f);
+                output->Damage[0].DamageMax = ((1.2f * float(ilevel)) * 1.15f) * (float(output->Delay) / 1000.0f);
                 output->Damage[0].DamageType = 0;
                 break;
             }
             case ITEM_SUBCLASS_WEAPON_WAND:
             {
                 output->Delay = (urand(12, 20, generator) * 100);
-                output->Damage[0].DamageMin = ((1.5f * float(ilevel)) * 0.85f) * (float(output->Delay) / 1000.0f);
-                output->Damage[0].DamageMax = ((1.5f * float(ilevel)) * 1.15f) * (float(output->Delay) / 1000.0f);
+                output->Damage[0].DamageMin = ((1.2f * float(ilevel)) * 0.85f) * (float(output->Delay) / 1000.0f);
+                output->Damage[0].DamageMax = ((1.2f * float(ilevel)) * 1.15f) * (float(output->Delay) / 1000.0f);
                 output->Damage[0].DamageType = urand(SPELL_SCHOOL_FIRE, SPELL_SCHOOL_ARCANE, generator);
                 break;
             }
