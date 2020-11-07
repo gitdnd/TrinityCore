e80a645db86fd494fb2404385cd55d3667370c37
Server <server@server.com>
2020-11-07 20:25:24 +0100

    Remove debug

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -737,15 +737,11 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
     // normal DB scripted effect
     TC_LOG_DEBUG("spells", "Spell ScriptStart spellid %u in EffectDummy(%u)", m_spellInfo->Id, effIndex);
     m_caster->GetMap()->ScriptsStart(sSpellScripts, uint32(m_spellInfo->Id | (effIndex << 24)), m_caster, unitTarget);
-
-    sWorld->SendGlobalText("DUMMY EFFECT", nullptr);
 #ifdef ELUNA
     if (gameObjTarget)
         sEluna->OnDummyEffect(m_caster, m_spellInfo->Id, effIndex, gameObjTarget);
-    else if (unitTarget && unitTarget->GetTypeId() == TYPEID_UNIT) {
-        sWorld->SendGlobalText("DUMMY EFFECT UNIT", nullptr);
+    else if (unitTarget && unitTarget->GetTypeId() == TYPEID_UNIT)
         sEluna->OnDummyEffect(m_caster, m_spellInfo->Id, effIndex, unitTarget->ToCreature());
-    }
     else if (itemTarget)
         sEluna->OnDummyEffect(m_caster, m_spellInfo->Id, effIndex, itemTarget);
 #endif
