7823913fca82ed87426a457ceb87b992d67dbdca
Server <server@server.com>
2020-11-28 14:09:18 +0100

    Temporarily revert to old system if cache is empty

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -26789,18 +26789,26 @@ float Player::GetAverageItemLevel() const
     float sum = 0;
     uint32 count = 0;
 
+    bool isCacheEmpty = _itemSlotToMaxLevel.size() == 0;
     for (uint8 i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; ++i)
     {
         // don't check tabard, ranged, offhand or shirt
         if (i == EQUIPMENT_SLOT_TABARD || i == EQUIPMENT_SLOT_RANGED || i == EQUIPMENT_SLOT_OFFHAND || i == EQUIPMENT_SLOT_BODY)
             continue;
 
-        /*if (m_items[i] && m_items[i]->GetTemplate())
+        // FIXME(Harry): Figured out why cache is empty initially on login
+        if (isCacheEmpty)
         {
-            sum += m_items[i]->GetTemplate()->GetItemLevel();
-        }*/
-        sum += float(_itemSlotToMaxLevel.at(i));
-
+            if (m_items[i] && m_items[i]->GetTemplate())
+            {
+                sum += m_items[i]->GetTemplate()->GetItemLevel();
+            }
+        }
+        else
+        {
+            sum += float(_itemSlotToMaxLevel.at(i));
+        }
+        
         // Add items with no slot to the count
         ++count;
     }
