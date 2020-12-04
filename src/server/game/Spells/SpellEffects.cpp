411d587a14bb32481d4adc279408a7bbf8e8d205
Server <server@server.com>
2020-12-04 19:28:06 +0100

    Experimental build, item upgrade to regenerate without upgrading

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5590,8 +5590,9 @@ void Spell::EffectVirtualItemQualityUpgrade(SpellEffIndex effIndex)
     VirtualModifier modifier;
     VirtualItemTemplate* vItem = sVirtualItemMgr.GetVirtualTemplate(itemTarget->GetEntry());
 
-    vItem->Quality = m_spellInfo->Effects[effIndex].MiscValue;
-    modifier.quality = m_spellInfo->Effects[effIndex].MiscValue;
+    //vItem->Quality = m_spellInfo->Effects[effIndex].MiscValue;
+    //modifier.quality = m_spellInfo->Effects[effIndex].MiscValue;
+    modifier.quality = vItem->Quality;
     modifier.seed = vItem->seed;
     modifier.displaySeed = vItem->displaySeed;
     modifier.nameSeed = vItem->nameSeed;
