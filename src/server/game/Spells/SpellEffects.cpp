90b94b765d10d6c8f3337b4bf35c8e7fd25a3ec5
Server <server@server.com>
2020-12-10 19:15:20 +0100

    Apply a large nerf to dungeon levels < 50

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -342,6 +342,9 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
             if (dungeonLevel > 0)
             {
                 float modifier = (pow(float(dungeonLevel), 2) / 40000.0f) + 0.5f;
+                if (dungeonLevel < 50) {
+                    modifier = modifier * 0.5;
+                }
                 damage = damage * modifier;
             }
         }
