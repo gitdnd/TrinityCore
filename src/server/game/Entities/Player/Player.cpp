643f8ead385ca01e39b0c081d4de20442f02b727
Server <server@server.com>
2021-01-12 21:30:32 +0100

    Force canTalk = true for debug

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -14427,7 +14427,8 @@ void Player::PrepareGossipMenu(WorldObject* source, uint32 menuId /*= 0*/, bool 
             }
         }
 
-        if (canTalk)
+        //if (canTalk)
+        if (true)
         {
             std::string strOptionText, strBoxText;
             BroadcastText const* optionBroadcastText = sObjectMgr->GetBroadcastText(itr->second.OptionBroadcastTextID);
