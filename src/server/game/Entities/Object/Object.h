7500f6c24bb4c50b17d112c91d5054a88ad1dc3d
Server <server@server.com>
2021-01-16 18:19:54 +0100

    Fix typo

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -328,7 +328,7 @@ class TC_GAME_API WorldObject : public Object, public WorldLocation
                 // if the target phase is 1 then allow to see
                 // otherwise phases must match
                 return (phasemask == 1 && CanSeePhaseOne()) ||
-                    (myMask == 1 && ToCreature() && CanSeePhaseOne) ||
+                    (myMask == 1 && ToCreature() && CanSeePhaseOne()) ||
                     myMask == phasemask ||
                     myMask == uint32(-1) ||
                     phasemask == uint32(-1);
