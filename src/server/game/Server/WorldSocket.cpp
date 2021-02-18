d8dc62ff73ad1b2c5a9f275505e678ebf03b8e19
Tb <tbgitemail@gmail.com>
2021-02-18 08:17:36 -0500

    Client isn't sending the build number we expect, log it.

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -496,6 +496,7 @@ void WorldSocket::HandleAuthSessionCallback(std::shared_ptr<AuthSession> authSes
     if (authSession->Build != realm.Build)
     {
         SendAuthResponseError(AUTH_VERSION_MISMATCH);
+        TC_LOG_ERROR("sql.sql", "Wrong build number got %u wanted %u", authSession->Build, realm.Build);
         DelayedCloseSocket();
         return;
     }
