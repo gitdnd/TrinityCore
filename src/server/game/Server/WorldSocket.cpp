7b6cab8ded767ffe54ce48091168ae275de155dd
Tb <tbgitemail@gmail.com>
2021-02-18 07:59:45 -0500

    Security: Check for version mismatch before connecting to realm.

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -428,6 +428,14 @@ void WorldSocket::HandleAuthSession(WorldPacket& recvPacket)
 
     // Read the content of the packet
     recvPacket >> authSession->Build;
+
+    if (authSession->Build != sWorld->getIntConfig(CONFIG_REALM_VERSION))
+    {
+        recvPacket.rfinish();
+        SendAuthResponseError(AUTH_VERSION_MISMATCH);
+        return;
+    }
+
     recvPacket >> authSession->LoginServerID;
     recvPacket >> authSession->Account;
     recvPacket >> authSession->LoginServerType;
