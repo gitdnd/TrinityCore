0392321672247a2fdd7e1d10f387b489deb4b4ed
Server <server@server.com>
2021-04-25 22:11:46 +0200

    Add debug damage check to bonus school aura handler

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -722,7 +722,7 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
         }
 
         // Apply bonus school damage effects
-        if (unitCaster)
+        if (unitCaster && m_spellInfo->Id != 11) // FIXME(Harry): Hardcoded check for not hardcoded spell
         {
             for (int i = 0; i < MAX_SPELL_SCHOOL; ++i)
             {
