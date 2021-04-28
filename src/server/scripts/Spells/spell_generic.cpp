686336af0ddafd32e1a8a7d8f76dcb4dfb838728
Server <server@server.com>
2021-04-28 22:33:07 +0200

    Remove debug code

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -41,7 +41,6 @@
 #include "SpellMgr.h"
 #include "SpellScript.h"
 #include "Vehicle.h"
-#include <World\World.h>
 
 class spell_gen_absorb0_hitlimit1 : public AuraScript
 {
@@ -4531,24 +4530,14 @@ public:
 
         bool CheckProc(ProcEventInfo& eventInfo)
         {
-            sWorld->SendGlobalText("spell_dmg_proc_aura: Spell Proc Occurred", nullptr);
-            
-            if (SpellInfo const* spellInfo = eventInfo.GetSpellInfo())
-                if (spellInfo->Id >= 1 && spellInfo->Id <= 5)
-                    return false;
-            
             return true;
         }
         void HandleProc(AuraEffect const* aurEff, ProcEventInfo& eventInfo)
         {
-            sWorld->SendGlobalText("spell_dmg_proc_aura: HandleProc", nullptr);
             if (SpellInfo const* spellInfo = eventInfo.GetSpellInfo())
             {
-                sWorld->SendGlobalText("spell_dmg_proc_aura: SpellInfo valid", nullptr);
-                uint32 school = spellInfo->Effects[0].MiscValue;
-                SpellSchools eschool = SpellSchools(school);
-                uint32 spell = spellInfo->Effects[0].TriggerSpell;
                 PreventDefaultAction();
+                uint32 spell = spellInfo->Effects[0].TriggerSpell;
                 uint32 proc_dmg = eventInfo.GetDamageInfo()->GetDamage() * (
                     (spellInfo->Effects[0].BasePoints + spellInfo->Effects[0].DieSides) / 100);
                 CastSpellExtraArgs args(aurEff);
