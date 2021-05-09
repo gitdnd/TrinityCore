923b4e74d798623904d7881a8fe8cad95a0acc7a
Server <server@server.com>
2021-05-09 15:42:43 +0200

    Slight change to Concentrate logic

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2378,6 +2378,14 @@ void Spell::TargetInfo::PreprocessTarget(Spell* spell)
             }
         }
     }
+    else
+    {
+        // Concentrate Talent implementation: On spell miss, proc
+        if (spell->m_caster->ToPlayer() && spell->m_caster->ToPlayer()->HasSpell(180118))
+        {
+            spell->m_caster->CastSpell(spell->m_caster, 180119);
+        }
+    }
 
     spell->CallScriptOnHitHandlers();
 
