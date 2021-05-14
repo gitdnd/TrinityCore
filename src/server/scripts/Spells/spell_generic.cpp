36ed94c0890466836d199f67b5b0bbdda61ccb2a
Server <server@server.com>
2021-05-15 00:29:23 +0200

    Implement spell_warlords_charge_periodic_aura AuraScript

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -4665,7 +4665,7 @@ class spell_verdant_dreamer_periodic_aura : public AuraScript
         if (!player)
             return;
 
-        int32 reduction = (player->GetStat(STAT_SPIRIT) * 0.05) * -1;
+        int32 reduction = (player->GetStat(STAT_SPIRIT) * 0.05f) * -1;
 
         CastSpellExtraArgs args(aurEff);
         args.OriginalCaster = GetCasterGUID();
@@ -4676,6 +4676,36 @@ class spell_verdant_dreamer_periodic_aura : public AuraScript
     void Register() override
     {
         OnEffectPeriodic += AuraEffectPeriodicFn(spell_verdant_dreamer_periodic_aura::PeriodicTick, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+    }
+};
+
+class spell_warlords_charge_periodic_aura : public AuraScript
+{
+    PrepareAuraScript(spell_warlords_charge_periodic_aura);
+
+    bool Validate(SpellInfo const* spellInfo) override
+    {
+        return ValidateSpellInfo({ spellInfo->Effects[EFFECT_0].TriggerSpell });
+    }
+
+    void PeriodicTick(AuraEffect const* aurEff)
+    {
+        PreventDefaultAction();
+        Player* player = GetTarget()->ToPlayer();
+        if (!player)
+            return;
+
+        int32 bonus = (player->GetStat(STAT_STAMINA) * 0.33f);
+
+        CastSpellExtraArgs args(aurEff);
+        args.OriginalCaster = GetCasterGUID();
+        args.AddSpellBP0(bonus);
+        player->CastSpell(player, 180150, args);
+    }
+
+    void Register() override
+    {
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_warlords_charge_periodic_aura::PeriodicTick, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
     }
 };
 
@@ -4817,4 +4847,5 @@ void AddSC_generic_spell_scripts()
     RegisterAuraScript(spell_second_wind_health_aura);
     RegisterAuraScript(spell_perseverance_health_aura);
     RegisterAuraScript(spell_verdant_dreamer_periodic_aura);
+    RegisterAuraScript(spell_warlords_charge_periodic_aura);
 }
