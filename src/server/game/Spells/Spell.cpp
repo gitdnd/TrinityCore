5efa6b9a7b503e332fb6bf819a41b586ae09eca7
Tb <tbsgitemail@gmail.com>
2021-05-19 20:11:38 -0400

    Change Item level error to no valid targets.

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7112,7 +7112,7 @@ SpellCastResult Spell::CheckItems(uint32* param1 /*= nullptr*/, uint32* param2 /
                     return SPELL_FAILED_NO_VALID_TARGETS;
 
                 if (m_spellInfo->Effects[i].BasePoints > 0 && m_spellInfo->Effects[i].BasePoints < m_targets.GetItemTarget()->GetTemplate()->ItemLevel)
-                    return SPELL_FAILED_LEVEL_REQUIREMENT;
+                    return SPELL_FAILED_NO_VALID_TARGETS;
 
                 break;
             }
