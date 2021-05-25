cf05b6d68546fbec86ddf2a4233ddb0da126964f
Server <server@server.com>
2021-05-25 21:07:49 +0200

    Fix missing space

--- src/server/database/Database/Implementation/CharacterDatabase.cpp
+++ src/server/database/Database/Implementation/CharacterDatabase.cpp
@@ -46,7 +46,7 @@ void CharacterDatabaseConnection::DoPrepareStatements()
                      "FLOOR((slotdata.head + slotdata.neck + slotdata.shoulders + slotdata.chest + slotdata.waist + slotdata.legs + slotdata.feet + slotdata.wrists + slotdata.hands + slotdata.finger1 + slotdata.finger2 + slotdata.trinket1 + slotdata.trinket2 + slotdata.back + slotdata.mainhand) / 15), "
                      "c.zone, c.map, c.position_x, c.position_y, c.position_z, "
                      "gm.guildid, c.playerFlags, c.at_login, cp.entry, cp.modelid, cp.level, c.equipmentCache, cb.guid "
-                     "FROM characters AS c"
+                     "FROM characters AS c "
                      "LEFT JOIN character_slot_max_level AS slotdata ON c.guid = slotdata.guid "
                      "LEFT JOIN character_pet AS cp ON c.guid = cp.owner AND cp.slot = ? LEFT JOIN guild_member AS gm ON c.guid = gm.guid "
                      "LEFT JOIN character_banned AS cb ON c.guid = cb.guid AND cb.active = 1 WHERE c.account = ? AND c.deleteInfos_Name IS NULL "
