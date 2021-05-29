dd70d1b5e0066f8e4bf23771beed3cad8ac26c7c
Server <server@server.com>
2021-05-29 18:50:42 +0200

    Include quality generation on upgrade

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5658,6 +5658,7 @@ void Spell::EffectVirtualItemQualityUpgrade(SpellEffIndex effIndex)
     modifier.statValueSeed = vItem->statValueSeed;
     modifier.ilevel = vItem->ItemLevel;
     modifier.statgroup = vItem->statGroup;
+    sVirtualItemMgr.GenerateQuality(vItem, modifier);
     sVirtualItemMgr.GenerateStats(vItem, modifier);
     sVirtualItemMgr.GenerateItemStats(vItem, modifier);
 
