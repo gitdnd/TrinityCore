d77921b91fb82019015b8c263aeca93abd5dcbbd
Server <server@server.com>
2021-05-29 18:21:48 +0200

    Try removing args from discord notifier

--- src/server/game/LuaEngine/ElunaUtility.h
+++ src/server/game/LuaEngine/ElunaUtility.h
@@ -34,7 +34,7 @@ inline void sendWebhook(Format&& fmt, Args&&... args)
 {
     std::ostringstream str;
     str << "C:\\HoT\\Development\\Server\\DiscordScriptError.exe";
-    str << " \"" << Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...) << "\"";
+    //str << " \"" << Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...) << "\"";
     std::system(str.str().c_str());
     //ShellExecute(NULL, "open", "C:\\HoT\\Development\\Server\\DiscordScriptError.exe", Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...).c_str(), NULL, SW_HIDE);
 }
