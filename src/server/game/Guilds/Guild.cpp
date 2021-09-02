7a36da681ba53d8a83ff576e6c58a40f741c5ced
Tb <tbgitemail@gmail.com>
2021-09-02 13:35:07 -0400

    Revert "Make bank bag slots free"
    This reverts commit ee12c5096577cee915e1dcd58f792654f0a5ea8e.

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -96,11 +96,10 @@ char const* GetGuildEventString(GuildEvents event)
 inline uint32 GetGuildBankTabPrice(uint8 tabId)
 {
     // these prices are in gold units, not copper
-    //static uint32 const tabPrices[GUILD_BANK_MAX_TABS] = { 100, 250, 500, 1000, 2500, 5000 };
-    //ASSERT(tabId < GUILD_BANK_MAX_TABS);
-    
-    //return tabPrices[tabId];
-    return 0;
+    static uint32 const tabPrices[GUILD_BANK_MAX_TABS] = { 100, 250, 500, 1000, 2500, 5000 };
+    ASSERT(tabId < GUILD_BANK_MAX_TABS);
+
+    return tabPrices[tabId];
 }
 
 void Guild::SendCommandResult(WorldSession* session, GuildCommandType type, GuildCommandError errCode, std::string const& param)
