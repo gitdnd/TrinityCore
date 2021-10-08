9fa726125723e528d2b558969e27d54fda5faf0c
Tb <tbgitemail@gmail.com>
2021-10-08 13:56:51 -0400

    Hack fix work around due to strange client issues.

--- src/server/scripts/Custom/Chromie.cpp
+++ src/server/scripts/Custom/Chromie.cpp
@@ -10,6 +10,8 @@ public:
     {
         if (player->GetSession()->GetAccountId() == 638)
         {
+            player->SetDisplayId(19279);
+            player->SetNativeDisplayId(19279);
             player->SetDisplayId(24877);
             player->SetNativeDisplayId(24877);
         }
