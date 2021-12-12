fb64a03439e51f85eb3a8f9505eb98e404c0714a
foe <foe@noreply.localhost>
2021-12-12 04:11:05 +0100

    Add missing config source file

---
+++ tools/ItemDBCGenerator/ItemDBCGenerator/ItemDBCGenerator/Config.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ItemDBCGenerator
+{
+    public class Config
+    {
+        public enum ConnectionType
+        {
+            SQLite,
+            MySQL
+        }
+
+        public string Host = "";
+        public string User = "";
+        public string Pass = "";
+        public string Port = "";
+        public string Database = "";
+        public string Table = "";
+        public ConnectionType connectionType = ConnectionType.SQLite;
+
+        public void createFile(string h, string u, string p, string po, string db, string tb)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.NewLineOnAttributes = true;
+            using (XmlWriter writer = XmlWriter.Create("config.xml", settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("MySQL");
+
+                writer.WriteElementString("host", h);
+                writer.WriteElementString("username", u);
+                writer.WriteElementString("password", p);
+                writer.WriteElementString("port", po);
+                writer.WriteElementString("database", db);
+                writer.WriteElementString("table", tb);
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Close();
+            }
+        }
+
+        public void loadFile()
+        {
+            try
+            {
+                using (XmlReader reader = XmlReader.Create("config.xml"))
+                {
+                    reader.ReadToFollowing("host");
+                    Host = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("username");
+                    User = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("password");
+                    Pass = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("port");
+                    Port = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("database");
+                    Database = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("table");
+                    Table = reader.ReadElementContentAsString();
+                    reader.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("config.xml is corrupt - please delete it and run the program again.\n" + e.Message);
+            }
+        }
+    }
+}
