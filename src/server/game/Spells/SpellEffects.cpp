582e94f40c83501520cd306cbe2827bcd3ce5538
stoneharry <na>
2022-01-23 00:32:35 +0100

    Change cancel to return

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3185,7 +3185,7 @@ void Spell::EffectWeaponDmg(SpellEffIndex effIndex)
             {
                 // Throw molotov and cancel current cast
                 player->CastSpell(unitTarget, 180168);
-                cancel();
+                return;
             }
         }
     }
