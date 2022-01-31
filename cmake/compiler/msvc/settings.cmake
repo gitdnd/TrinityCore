5cf152b7b2eb126aeeaf5a4a874119c8cf15259d
Tb <tbgitemail@gmail.com>
2022-01-30 21:59:14 -0500

    Disable permissive mode.

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -22,9 +22,9 @@ target_compile_options(trinity-warning-interface
     /W3)
 
 # disable permissive mode to make msvc more eager to reject code that other compilers don't already accept
-target_compile_options(trinity-compile-option-interface
-  INTERFACE
-    /permissive-)
+#target_compile_options(trinity-compile-option-interface
+#  INTERFACE
+#    /permissive-)
 
 # set up output paths ofr static libraries etc (commented out - shown here as an example only)
 #set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
