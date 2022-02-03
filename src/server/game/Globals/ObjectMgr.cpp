437d8e50112308c6227465f5fe1f84223659948b
stoneharry <na>
2022-02-03 01:16:57 +0100

    Adjust Talent XP Required rate

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -7938,7 +7938,7 @@ uint32 ObjectMgr::GetBaseXP(uint8 level)
 uint32 ObjectMgr::GetXPForLevel(uint32 level) const
 {
     // ((ilevel ^ 1.35) / 2) * 1000
-    return (std::pow(level, 1.35) * 0.5) * 1000;
+    return (std::pow(level, 1.5) * 0.5) * 1000;
     /*if (level < _playerXPperLevel.size())
        return _playerXPperLevel[level];
     return 0;*/
