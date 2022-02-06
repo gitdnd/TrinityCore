678474a23aff73223682f89363695a3bfb8efb4a
stoneharry <na>
2022-02-06 17:39:36 +0100

    Fix typo

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -4904,7 +4904,7 @@ class spell_talent_engulfing_flames_aura : public AuraScript
 
     void Register() override
     {
-        OnApply += AuraEffectApplyFn(spell_talent_engulfing_flames_aura::OnApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+        OnEffectApply += AuraEffectApplyFn(spell_talent_engulfing_flames_aura::OnApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
     }
 };
 
