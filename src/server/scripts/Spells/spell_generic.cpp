985b22307cd0dcdf6a33f790a354b020b8915868
stoneharry <na>
2022-02-12 20:39:54 +0100

    Implement 180204 Solar Flare Immune

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -5029,10 +5029,12 @@ class spell_talent_from_the_ashes_resurrection_aura : public AuraScript
             return;
         if (target->getDeathState() == JUST_DIED)
         {
-            if (caster->isDead() && caster->ToPlayer())
+            // 180204 Solar Flare Immune (5min debuff after ress)
+            if (caster->isDead() && caster->ToPlayer() && !caster->HasAura(180204))
             {
                 caster->ToPlayer()->ResurrectPlayer(0.2);
                 caster->CastSpell(caster, 24171);
+                caster->CastSpell(caster, 180204);
             }
         }
     }
