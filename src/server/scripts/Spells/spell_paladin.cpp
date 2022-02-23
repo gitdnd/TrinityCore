8b3f4e5ff50f552ef89af5b03f3d8b7c6d2fa08f
stoneharry <na>
2022-02-23 23:12:22 +0100

    Update Beacon of Light to ignore AoE spells

--- src/server/scripts/Spells/spell_paladin.cpp
+++ src/server/scripts/Spells/spell_paladin.cpp
@@ -1775,6 +1775,8 @@ class spell_pal_light_s_beacon : public SpellScriptLoader
             {
                 if (GetTarget()->HasAura(SPELL_PALADIN_BEACON_OF_LIGHT, eventInfo.GetActor()->GetGUID()))
                     return false;
+                if (eventInfo.GetSpellInfo()->Effects->CalcRadius() > 0)
+                    return false;
                 return true;
             }
 
