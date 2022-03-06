d670fe246b5b18310a727257bef41a0c4fbd164d
Foereaper <foereaper@elunatech.com>
2022-03-06 02:41:14 +0100

    Make a primary stat always roll

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -659,6 +659,8 @@ void VirtualItemMgr::GenerateItemStatsNew(VirtualItemTemplate* output, VirtualMo
     // check whether or not the amount of stats exceeds the size of our stat group
     if ((primaryStatSlots + primarySlotMod) > primarystatgroup.size())
         primaryStatSlots = primarystatgroup.size();
+    if ((primaryStatSlots + primarySlotMod) < 1)
+        primaryStatSlots = 1;
     else
         primaryStatSlots += primarySlotMod;
 
