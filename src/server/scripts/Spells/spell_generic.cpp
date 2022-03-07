a31796e4c18670064578c5460492b8c032d080af
stoneharry <na>
2022-03-07 19:27:08 +0100

    Implement spell_talent_congelation_trigger_aura and spell_talent_congelation_actual_aura

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -5300,6 +5300,54 @@ class spell_evokers_intellect_aura : public AuraScript
     std::vector<uint32> uniqueSpells;
 };
 
+class spell_talent_congelation_trigger_aura : public AuraScript
+{
+    PrepareAuraScript(spell_talent_congelation_trigger_aura);
+
+    void OnProc(AuraEffect const* /*aurEff*/, ProcEventInfo& /*eventInfo*/)
+    {
+        auto caster = GetCaster();
+        if (!caster)
+        {
+            PreventDefaultAction();
+            return;
+        }
+        if (caster->GetHealthPct() > 35)
+        {
+            PreventDefaultAction();
+        }
+        // Trigger
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_talent_congelation_trigger_aura::OnProc, EFFECT_0, SPELL_AURA_PROC_TRIGGER_SPELL);
+    }
+};
+
+class spell_talent_congelation_actual_aura : public AuraScript
+{
+    PrepareAuraScript(spell_talent_congelation_actual_aura);
+
+    void OnProc(AuraEffect const* aurEff, ProcEventInfo& /*eventInfo*/)
+    {
+        PreventDefaultAction();
+        if (GetCaster() && GetCaster()->GetHealthPct() > 35)
+        {
+            auto base = aurEff->GetBase();
+            if (base)
+            {
+                base->Remove();
+            }
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_talent_congelation_actual_aura::OnProc, EFFECT_2, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+    }
+};
+
 void AddSC_generic_spell_scripts()
 {
     RegisterAuraScript(spell_gen_absorb0_hitlimit1);
@@ -5454,6 +5502,8 @@ void AddSC_generic_spell_scripts()
     RegisterAuraScript(spell_talent_heart_glacier_aura);
     RegisterAuraScript(spell_talent_polar_affliction_aura);
     RegisterAuraScript(spell_talent_hammer_of_the_north_aura);
+    RegisterAuraScript(spell_talent_congelation_trigger_aura);
+    RegisterAuraScript(spell_talent_congelation_actual_aura);
     new spell_generate_combopoint_with_aura("spell_gen_generate_combo_point_forst", 180057);
     /*new spell_generate_combopoint_with_aura_array("spell_gen_generate_combo_point_dummy", { 1, 2, 3, 4, 5 });*/
     RegisterAuraScript(spell_evokers_intellect_aura);
