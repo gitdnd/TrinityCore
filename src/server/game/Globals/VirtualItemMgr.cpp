35f105cffa9b553d8d80c5338c51c1fe0ce37f1a
Foereaper <foereaper@elunatech.com>
2022-03-09 21:15:24 +0100

    Disable ilevel check for debugging

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -1069,11 +1069,11 @@ itemSpellInfo VirtualItemMgr::GenerateSpell(VirtualItemTemplate* output, Virtual
         if (someSpells.statGroup != -1 && output->statGroup != someSpells.statGroup)
             continue;
 
-        if (someSpells.maxItemLevel != -1 && output->ItemLevel > (uint32)someSpells.maxItemLevel)
-            continue;
+        //if (someSpells.maxItemLevel != -1 && (int32)output->ItemLevel > someSpells.maxItemLevel)
+        //    continue;
 
-        if (someSpells.minItemLevel != -1 && output->ItemLevel < (uint32)someSpells.minItemLevel)
-            continue;
+        //if (someSpells.minItemLevel != -1 && (int32)output->ItemLevel < someSpells.minItemLevel)
+        //    continue;
 
         spells.push_back(someSpells);
     }
