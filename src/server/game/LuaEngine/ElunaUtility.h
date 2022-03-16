d78d888b7e391d1fb412a683bd6a9261205bffa6
Tb <tbgitemail@gmail.com>
2022-03-16 12:40:40 -0400

    Wrap webhook around development hook.

--- src/server/game/LuaEngine/ElunaUtility.h
+++ src/server/game/LuaEngine/ElunaUtility.h
@@ -30,10 +30,13 @@
 template<typename Format, typename... Args>
 inline void sendWebhook(Format&& fmt, Args&&... args)
 {
-    std::ostringstream str;
-    str << "DiscordScriptError.exe";
-    str << " \"" << Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...) << "\"";
-    std::system(str.str().c_str());
+    if (sWorld->setBoolConfig(CONFIG_ALLOW_DEVELOPMENT))
+    {
+        std::ostringstream str;
+        str << "DiscordScriptError.exe";
+        str << " \"" << Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...) << "\"";
+        std::system(str.str().c_str());
+    }
     //std::system("C:\\HoT\\Development\\Server\\DiscordScriptError.exe");
     //ShellExecute(NULL, "open", "C:\\HoT\\Development\\Server\\DiscordScriptError.exe", Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...).c_str(), NULL, SW_HIDE);
 }
