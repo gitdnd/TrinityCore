06dcd66daee8f6169d36b89f33d3e66504771054
Foereaper <foereaper@elunatech.com>
2022-04-13 17:16:35 +0200

    Fix virtual item saving and loading

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -3444,7 +3444,8 @@ void ObjectMgr::LoadVirtualItemTemplates()
         "spellid_3, spelltrigger_3, spellcharges_3, spellppmRate_3, spellcooldown_3, spellcategory_3, spellcategorycooldown_3, "
         "spellid_4, spelltrigger_4, spellcharges_4, spellppmRate_4, spellcooldown_4, spellcategory_4, spellcategorycooldown_4, "
         "spellid_5, spelltrigger_5, spellcharges_5, spellppmRate_5, spellcooldown_5, spellcategory_5, spellcategorycooldown_5, "
-        "seed, socketSeed, qualitySeed, statSeed, nameSeed, displaySeed, spellSeed, statValueSeed, statGroupSeed, statGroup, customFlags, sheath, legendarySeed, legendaryId "
+        "seed, socketSeed, qualitySeed, statSeed, nameSeed, displaySeed, spellSeed, statValueSeed, statGroupSeed, setSeed, legendarySeed, "
+        "statGroup, customFlags, sheath, legendaryId "
         "FROM item_template_virtual");
 
     if (!result)
