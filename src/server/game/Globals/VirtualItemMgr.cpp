9a32deb433a5f355117c6d641f30551305d59fd4
Tb <tbgitemail@gmail.com>
2022-04-14 23:56:31 -0400

    Revert "Move stat count override spot."
    This reverts commit 50a9862d59d7c666c55faddd1a4e1d8bb0309cff.

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -563,6 +563,14 @@ void VirtualItemMgr::GenerateItemStats(VirtualItemTemplate* output, VirtualModif
             secondaryStatSlots = 1;
     }
 
+    if (legendaryItemInfo const* leg = GetLegendaryItemInfo(output->legendaryId))
+    {
+        if (leg->primaryStatCountMod)
+            primaryStatSlots = leg->primaryStatCountMod;
+
+        if (leg->secondaryStatCountMod)
+            secondaryStatSlots = leg->secondaryStatCountMod;
+    }
 
     // multiply the pool size by the base amounts of stat slots
     pool *= (float)(primaryStatSlots + secondaryStatSlots);
@@ -589,14 +597,6 @@ void VirtualItemMgr::GenerateItemStats(VirtualItemTemplate* output, VirtualModif
             secondaryStatSlots += secondarySlotMod;
     }
 
-    if (legendaryItemInfo const* leg = GetLegendaryItemInfo(output->legendaryId))
-    {
-        if (leg->primaryStatCountMod)
-            primaryStatSlots = leg->primaryStatCountMod;
-
-        if (leg->secondaryStatCountMod)
-            secondaryStatSlots = leg->secondaryStatCountMod;
-    }
 
     // if we still have any slots to generate stats for, continue
     if (primaryStatSlots + secondaryStatSlots > 0)
