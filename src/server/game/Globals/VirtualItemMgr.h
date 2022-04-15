146b5a40ae6eb1cf8ac99c9ceb94ea88661724c9
Foereaper <foereaper@elunatech.com>
2022-04-15 02:20:34 +0200

    Remove statgroup enum cast

--- src/server/game/Globals/VirtualItemMgr.h
+++ src/server/game/Globals/VirtualItemMgr.h
@@ -103,7 +103,7 @@ struct VirtualModifier
         ilevel = 0;
         quality = MAX_ITEM_QUALITY;
         statpool = -1;
-        statgroup = StatGroup(STAT_GROUP_RANDOM);
+        statgroup = STAT_GROUP_RANDOM;
         seed = 0;
         socketSeed = 0;
         qualitySeed = 0;
