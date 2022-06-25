ed20f388e2a271b53b283b30f13fede2d3a95673
Tb <tbgitemail@gmail.com>
2022-06-24 20:21:17 -0400

    Make visual triggered full.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2612,9 +2612,7 @@ void Player::GiveXP(uint32 xp, Unit* victim, float group_rate)
         if (talent_level + 1 <= sWorld->getIntConfig(CONFIG_MAX_TALENT_LEVEL))
         {
             ++talent_level;
-            CastSpellExtraArgs args = CastSpellExtraArgs();
-            args.SetTriggerFlags(TriggerCastFlags(TRIGGERED_IGNORE_CAST_IN_PROGRESS));
-            CastSpell(this, 90299, args); // Talent level up visual
+            CastSpell(this, 90299, true); // Talent level up visual
             SetUInt32Value(PLAYER_NEXT_LEVEL_XP, sObjectMgr->GetXPForLevel(talent_level));
             InitTalentForLevel();
             ChatHandler(GetSession()).SendSysMessage(("Your talent level has increased to " + std::to_string(talent_level) + ".").c_str());
