626567df4f545fec005b700660bd470f45d0a137
Tb <tbgitemail@gmail.com>
2022-07-22 17:21:15 -0400

    Add fall of dalaran instance script.

--- src/server/scripts/Custom/Instances/instance_dummies.cpp
+++ src/server/scripts/Custom/Instances/instance_dummies.cpp
@@ -22,7 +22,28 @@ public:
     }
 };
 
+class instance_fall_of_dalaran : public InstanceMapScript
+{
+public:
+    instance_fall_of_dalaran() : InstanceMapScript("instance_fall_of_dalaran", 560) { }
+
+    struct instance_fall_of_dalaran_InstanceMapScript : public InstanceScript
+    {
+        instance_fall_of_dalaran_InstanceMapScript(Map* map) : InstanceScript(map)
+        {
+            SetHeaders("FD");
+            SetBossNumber(5);
+        }
+    };
+
+    InstanceScript* GetInstanceScript(InstanceMap* map) const override
+    {
+        return new instance_fall_of_dalaran_InstanceMapScript(map);
+    }
+};
+
 void AddSC_Instance_Dummies()
 {
     new instance_kara_crypts();
+    new instance_fall_of_dalaran();
 }
