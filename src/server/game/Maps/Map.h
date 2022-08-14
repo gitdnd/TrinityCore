b14cd30179053756728277e2e35f087d1e70a78e
Tb <tbgitemail@gmail.com>
2022-08-14 17:55:46 -0400

    remote branch.

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -41,6 +41,7 @@
 class Battleground;
 class BattlegroundMap;
 class CreatureGroup;
+class Eluna;
 class GameObjectModel;
 class Group;
 class InstanceMap;
@@ -320,6 +321,7 @@ inline bool CompareRespawnInfo::operator()(RespawnInfo const* a, RespawnInfo con
 class TC_GAME_API Map : public GridRefManager<NGridType>
 {
     friend class MapReference;
+    friend class Eluna;
     public:
         Map(uint32 id, time_t, uint32 InstanceId, uint8 SpawnMode, int dungeonLevel, Map* _parent = nullptr);
         virtual ~Map();
@@ -647,6 +649,8 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
 
         virtual std::string GetDebugInfo() const;
 
+        Eluna* GetEluna() const { return eluna; }
+        Eluna* eluna;
     private:
         void LoadMapAndVMap(int gx, int gy);
         void LoadVMap(int gx, int gy);
@@ -700,7 +704,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         void ScriptsProcess();
 
         void SendObjectUpdates();
-
+        
     protected:
         void SetUnloadReferenceLock(GridCoord const& p, bool on) { getNGrid(p.x_coord, p.y_coord)->setUnloadReferenceLock(on); }
 
@@ -728,6 +732,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         typedef std::set<Transport*> TransportsContainer;
         TransportsContainer _transports;
         TransportsContainer::iterator _transportsUpdateIter;
+        
 
     private:
         Player* _GetScriptPlayerSourceOrTarget(Object* source, Object* target, ScriptInfo const* scriptInfo) const;
@@ -903,7 +908,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
 
         std::unordered_set<Object*> _updateObjects;
 
-        MPSCQueue<FarSpellCallback> _farSpellCallbacks;
+        MPSCQueue<FarSpellCallback> _farSpellCallbacks
 };
 
 enum InstanceResetMethod
