807784e7e8d390d30cba70bb65a66fe500f5bbad
Foereaper <foereaper@elunatech.com>
2022-08-17 23:06:44 +0200

    Add beginning framework of global script loader

---
+++ src/server/game/LuaEngine/ElunaLoader.h
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2010 - 2022 Eluna Lua Engine <https://elunaluaengine.github.io/>
+* Copyright (C) 2022 - 2022 Hour of Twilight <https://www.houroftwilight.net/>
+* This program is free software licensed under GPL version 3
+* Please see the included DOCS/LICENSE.md for more information
+*/
+
+
+namespace ElunaLoader
+{
+    void LoadScripts();
+    static void ReadFiles(std::string path);
+
+    // Lua script folder path
+    static std::string lua_folderpath;
+    // lua path variable for require() function
+    static std::string lua_requirepath;
+
+    struct lua_info
+    {
+        lua_info(const char* content, const char* name, const char* extension) {
+            this->script_content = content;
+            this->script_name = name;
+            this->script_extension = extension;
+        }
+
+        const char* script_content;
+        const char* script_name;
+        const char* script_extension;
+    };
+
+    std::vector<lua_info> Scripts;
+}
