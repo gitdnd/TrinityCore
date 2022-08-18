9b382bc85b2e61355f02ef5537f1b5f3619b5a30
Foereaper <foereaper@elunatech.com>
2022-08-19 01:28:47 +0200

    Fix teleport to non-lua map

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -1835,9 +1835,9 @@ void WorldObject::SetMap(Map* map)
     m_InstanceId = map->GetInstanceId();
 
 #ifdef ELUNA
+    delete elunaEvents;
     if (GetMap()->GetEluna())
     {
-        delete elunaEvents;
         // On multithread replace this with a pointer to map's Eluna pointer stored in a map
         elunaEvents = new ElunaEventProcessor(GetMap()->GetEluna(), this);
     }
