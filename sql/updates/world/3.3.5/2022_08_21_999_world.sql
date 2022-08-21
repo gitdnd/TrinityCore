52ff311f4625ad30c7d9b7ab16cac5312705eb48
ReynoldsCahoon <braden.beer@gmail.com>
2022-08-21 01:44:29 -0600

    Added player gem selection data

---
+++ sql/updates/world/3.3.5/2022_08_21_999_world.sql
@@ -0,0 +1,50 @@
+-- Wipe Previous Data
+delete from `player_gemselection`;
+
+-- Defender
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70312, 171312, 49576); -- Death Grip
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70057, 171057, 7301); -- Frost Armor
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70003, 171003, 355); -- Taunt
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70013, 171013, 696); -- Demon Skin
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70198, 171198, 47450); -- Heroic Strike
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (1, 70059, 171059, 7386); -- Sunder Armor
+
+-- Diviner
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70269, 171269, 48782); -- Holy Light
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70350, 171350, 55459); -- Chain Heal
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70231, 171231, 48068); -- Renew
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70254, 171254, 48443); -- Regrowth
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70409, 171409, 48451); -- Lifebloom
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (2, 70230, 171230, 48066); -- Power Word: Shield
+
+-- Brawler
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70288, 171288, 48996); -- Raptor Strike
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70076, 171076, 11578); -- Charge
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70259, 171259, 48638); -- Sinister Strike
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70264, 171264, 48668); -- Eviscerate
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70065, 171065, 8643); -- Kidney Shot
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (3, 70205, 171205, 47520); -- Cleave
+
+-- Ranger
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70015, 171015, 781); -- Disengage
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70294, 171294, 49045); -- Arcane Shot
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70048, 171048, 5116); -- Concussive Shot
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70296, 171296, 49050); -- Aimed Shot
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70295, 171295, 49048); -- Multi-Shot
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (4, 70041, 171041, 3034); -- Viper Sting
+
+-- Sorcerer
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70160, 171160, 48135); -- Holy Fire
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70222, 171222, 47864); -- Curse of Agony
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70406, 171406, 48461); -- Wrath
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70166, 171166, 42873); -- Fire Blast
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70170, 171170, 42917); -- Frost Nova
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (5, 70171, 171171, 42921); -- Arcane Explosion
+
+-- Support
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70286, 171286, 48950); -- Redemption
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70280, 171280, 48936); -- Blessing of Wisdom
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70052, 171052, 5384); -- Feign Death
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70072, 171072, 10890); -- Psychic Scream
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70087, 171087, 12826); -- Polymorph
+insert into `player_gemselection` (`category`, `gemId`, `skillId`, `spellId`) values (6, 70081, 171081, 12051); -- Evocation
