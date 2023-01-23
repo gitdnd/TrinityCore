eb12669ec965285fa766672e41d22d663afc8a33
Foereaper <foereaper@elunatech.com>
2023-01-23 20:39:43 +0100

    add to debug output

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -1321,11 +1321,11 @@ public:
                     char const* setStr = strtok(nullptr, " ");
                     if (setStr)
                     {
-                        modifier.generateSet = (setStr == "1");
+                        modifier.generateSet = (atoi(setStr) == 1);
                     }
                 }
             }
-            handler->PSendSysMessage("ItemId = %d, seed = %d, ilevel = %d, quality = %d", itemId, modifier.seed, modifier.ilevel, (int)modifier.quality);
+            handler->PSendSysMessage("ItemId = %d, seed = %d, ilevel = %d, quality = %d, setOverride = %d", itemId, modifier.seed, modifier.ilevel, (int)modifier.quality, int(modifier.generateSet));
         }
 
         Item* item = playerTarget->StoreNewItem3(dest, itemId, true, GenerateItemRandomPropertyId(itemId), GuidSet(), modifier);
