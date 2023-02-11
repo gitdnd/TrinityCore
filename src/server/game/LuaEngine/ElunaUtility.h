780a55972c19b09ca7605fff50ef9117d6478904
Tb <tbgitemail@gmail.com>
2023-02-11 16:16:29 -0500

    Enabled discord script webhook in live.

--- src/server/game/LuaEngine/ElunaUtility.h
+++ src/server/game/LuaEngine/ElunaUtility.h
@@ -47,13 +47,13 @@
 template<typename Format, typename... Args>
 inline void sendWebhook(Format&& fmt, Args&&... args)
 {
-    if (sWorld->getBoolConfig(CONFIG_ALLOW_DEVELOPMENT))
-    {
+    //if (sWorld->getBoolConfig(CONFIG_ALLOW_DEVELOPMENT))
+    //{
         std::ostringstream str;
         str << "DiscordScriptError.exe";
         str << " \"" << Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...) << "\" &";
         std::system(str.str().c_str());
-    }
+    //}
     //std::system("C:\\HoT\\Development\\Server\\DiscordScriptError.exe");
     //ShellExecute(NULL, "open", "C:\\HoT\\Development\\Server\\DiscordScriptError.exe", Trinity::StringFormat(std::forward<Format>(fmt), std::forward<Args>(args)...).c_str(), NULL, SW_HIDE);
 }
