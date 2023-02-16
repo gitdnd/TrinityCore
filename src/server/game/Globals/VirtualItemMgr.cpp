8da92bb0256d22eb1126383eb13bc293c8c235ed
Tb <tbgitemail@gmail.com>
2023-02-16 17:29:19 -0500

    debug?

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -2108,11 +2108,14 @@ void VirtualItemMgr::GenerateLegendaryItemEffect(VirtualItemTemplate* output, Vi
             continue;
         if (SelectSkipDebug(itr.second.itemStatGroup, output->statGroup, "[StatGroup]"))
             continue;
-        for (uint8 i = 1; i < MAX_LEGENDARY_SPELLS; ++i)
+        bool skip = false;
+        for (uint8 i = 0; i < MAX_LEGENDARY_SPELLS; ++i)
         {
             if (itr.second.legendarySpells[i].SpellId > 0 && itr.second.legendarySpells[i].SpellTrigger == ITEM_SPELLTRIGGER_ON_USE && hasExistingOnUse)
-                continue;
+                skip = true;
         }
+        if (skip)
+            continue;
         legList.push_back(itr.second);
     }
 
