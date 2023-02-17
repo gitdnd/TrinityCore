0c5c0e2874c261335182d468aae3c49813e3a833
foe <foe@noreply.localhost>
2023-02-17 20:25:52 +0100

    Revert deathbringer changes
    Review this later on for archetypes

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -586,25 +586,39 @@ class spell_item_deathbringers_will : public SpellScriptLoader
 
             void HandleProc(AuraEffect const* aurEff, ProcEventInfo& eventInfo)
             {
-                static std::vector<uint32> const triggeredSpells[6] =
+                static std::vector<uint32> const triggeredSpells[MAX_CLASSES] =
                 {
-                    //Timewalker
-                    { Strength, Agility, AttackPower, Critical, Haste},
-                    //CLASS_SUB_WARDEN
+                    //CLASS_NONE
+                    { },
+                    //CLASS_WARRIOR
                     { Strength, Critical, Haste },
-                    //CLASS_SUB_HISTORIAN
-                    { Critical, Haste },
-                    //CLASS_SUB_WEAVER
-                    { Critical, Haste },
-                    //CLASS_SUB_WATCHER
-                    { Agility, Strength, Critical, Haste, AttackPower },
-                    //CLASS_SUB_RANGER
-                   { Agility, Critical, Haste, AttackPower },
+                    //CLASS_PALADIN
+                    { Strength, Critical, Haste },
+                    //CLASS_HUNTER
+                    { Agility, Critical, AttackPower },
+                    //CLASS_ROGUE
+                    { Agility, Haste, AttackPower },
+                    //CLASS_PRIEST
+                    { },
+                    //CLASS_DEATH_KNIGHT
+                    { Strength, Critical, Haste },
+                    //CLASS_SHAMAN
+                    { Agility, Haste, AttackPower },
+                    //CLASS_MAGE
+                    { },
+                    //CLASS_WARLOCK
+                    { },
+                    //CLASS_UNK
+                    { },
+                    //CLASS_DRUID
+                    { Strength, Agility, Haste },
+                    // Timewalker
+                    { Strength, Agility, AttackPower, Critical, Haste},
                 };
 
                 PreventDefaultAction();
-                Player* caster = eventInfo.GetActor()->ToPlayer();
-                std::vector<uint32> const& randomSpells = triggeredSpells[caster->GetSubOrClass() - CLASS_TIMEWALKER];
+                Unit* caster = eventInfo.GetActor();
+                std::vector<uint32> const& randomSpells = triggeredSpells[caster->GetClass()];
                 if (randomSpells.empty())
                     return;
 
