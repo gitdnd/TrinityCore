54ceb00fa84e987ea21c7b69620fcbc63345413b
Foereaper <foereaper@elunatech.com>
2023-05-11 18:39:27 +0200

    Remove map processor if check entirely

--- src/server/game/LuaEngine/ServerHooks.cpp
+++ src/server/game/LuaEngine/ServerHooks.cpp
@@ -337,12 +337,9 @@ void Eluna::OnPlayerLeave(Map* map, Player* player)
 
 void Eluna::OnUpdate(Map* map, uint32 diff)
 {
-    // only update the globalProcessor if the map being updated is not the parent map
-    if (!map->IsParent())
-    {
-        eventMgr->globalProcessor->Update(diff);
-        GetQueryProcessor().ProcessReadyCallbacks();
-    }
+    eventMgr->globalProcessor->Update(diff);
+    GetQueryProcessor().ProcessReadyCallbacks();
+
     START_HOOK(MAP_EVENT_ON_UPDATE);
     Push(map);
     Push(diff);
