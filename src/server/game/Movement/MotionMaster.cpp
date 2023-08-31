b5c597599dd9f06ec8fef77fa77070cbe8b75e32
stoneharry <stoneharryjames@gmail.com>
2023-08-31 23:27:02 +0100

    Revert "Try setting BaseUnitState UNIT_STATE_CANNOT_TURN"
    This reverts commit 006914b4881d545d7b2a6de8c64be9316a8299bf.

--- src/server/game/Movement/MotionMaster.cpp
+++ src/server/game/Movement/MotionMaster.cpp
@@ -890,7 +890,7 @@ void MotionMaster::MoveJump(float x, float y, float z, float o, float speedXY, f
 
     GenericMovementGenerator* movement = new GenericMovementGenerator(std::move(init), EFFECT_MOTION_TYPE, id);
     movement->Priority = MOTION_PRIORITY_HIGHEST;
-    movement->BaseUnitState = hasOrientation ? UNIT_STATE_CANNOT_TURN : UNIT_STATE_JUMPING;
+    movement->BaseUnitState = UNIT_STATE_JUMPING;
     Add(movement);
 }
 
