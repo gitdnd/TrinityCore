7e0e0a6b70c04548f417481feb858f40d702f70a
Tb <tbgitemail@gmail.com>
2023-09-01 16:55:35 -0400

    More tests

--- src/server/game/LuaEngine/GlobalMethods.h
+++ src/server/game/LuaEngine/GlobalMethods.h
@@ -3289,42 +3289,22 @@ namespace LuaGlobalFunctions
 
     int GetCustomTalentStorage(Eluna* E)
     {
+        lua_State* L = E->L;
         lua_newtable(E->L);
         for (auto const& itr : sObjectMgr->GetTalentNodeStore())
         {
             // use index as subtable key
-            lua_pushnumber(E->L, itr.second.Index);
-
-            // create subtable
-            lua_newtable(E->L);
-
-            // push key 1 and value, n...
-            lua_pushnumber(E->L, 1);
-            lua_pushnumber(E->L, itr.second.spellId);
-
-            lua_pushnumber(E->L, 2);
-            lua_pushnumber(E->L, itr.second.xOffset);
-
-            lua_pushnumber(E->L, 3);
-            lua_pushnumber(E->L, itr.second.yOffset);
-
-            lua_pushnumber(E->L, 4);
-            lua_pushnumber(E->L, itr.second.Mutex);
-
-            lua_pushnumber(E->L, 5);
-            lua_pushnumber(E->L, itr.second.buttonType);
-
-            lua_pushnumber(E->L, 6);
-            lua_pushnumber(E->L, itr.second.flagMask);
-
-            lua_pushnumber(E->L, 7);
-            lua_pushstring(E->L, itr.second.link_str.c_str());
-
-            // set the subtable
-            lua_settable(E->L, -15);
-
-            // set the main table
-            lua_settable(E->L, -3);
+            lua_pushnumber(L, itr.second.Index);
+            lua_settable(L, -2);
+            lua_newtable(L);
+            lua_pushnumber(L, itr.second.spellId);
+            lua_pushnumber(L, itr.second.xOffset);
+            lua_pushnumber(L, itr.second.yOffset);
+            lua_pushnumber(L, itr.second.Mutex);
+            lua_pushnumber(L, itr.second.buttonType);
+            lua_pushnumber(L, itr.second.flagMask);
+            lua_pushstring(L, itr.second.link_str.c_str());
+            lua_settable(L, -10);
         }
 
         /*lua_createtable(E->L, sObjectMgr->GetTalentNodeStore().size(), 0);
