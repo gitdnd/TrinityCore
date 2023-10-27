a0fe33f15850f826287c7b816dbf11dd8985f47c
stoneharry <stoneharryjames@gmail.com>
2023-10-27 20:42:42 +0100

    Add flat 25% increase in xp gain

--- src/server/game/Entities/Player/KillRewarder.cpp
+++ src/server/game/Entities/Player/KillRewarder.cpp
@@ -156,6 +156,8 @@ inline void KillRewarder::_RewardXP(Player* player, float rate)
         // 4.2.2. Apply auras modifying rewarded XP (SPELL_AURA_MOD_XP_PCT).
         xp *= player->GetTotalAuraMultiplier(SPELL_AURA_MOD_XP_PCT);
 
+        xp = xp * 1.25; // Flat 25% xp bonus
+
         if (_group)
         {
             // Nerfs boosting xp by running with low level alts
