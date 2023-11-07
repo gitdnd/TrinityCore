bb400366c5a23c4f1cf3b0dee8783c112fd40f2c
itswicky <blaindes51@gmail.com>
2023-11-07 14:05:44 -0600

    Core/Spell: Allow SPELL_AURA_PERIODIC_ENERGIZE to restore Focus to players

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5607,8 +5607,9 @@ void AuraEffect::HandlePeriodicEnergizeAuraTick(Unit* target, Unit* caster) cons
 {
     Powers powerType = Powers(GetMiscValue());
 
-    if (target->GetTypeId() == TYPEID_PLAYER && target->GetPowerType() != powerType && !m_spellInfo->HasAttribute(SPELL_ATTR7_CAN_RESTORE_SECONDARY_POWER))
-        return;
+    // Allow SPELL_AURA_PERIODIC_ENERGIZE to restore Focus to players --Itswicky 
+    /*if (target->GetTypeId() == TYPEID_PLAYER && target->GetPowerType() != powerType && !m_spellInfo->HasAttribute(SPELL_ATTR7_CAN_RESTORE_SECONDARY_POWER))
+        return;*/
 
     if (!target->IsAlive() || !target->GetMaxPower(powerType))
         return;
