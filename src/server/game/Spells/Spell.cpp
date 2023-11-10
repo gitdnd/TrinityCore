9db9fe2b86cc6e5ebfe1a5700c6bdcd181d5cfc5
Foereaper <foereaper@elunatech.com>
2023-11-10 18:15:54 +0100

    Fix ranged damage school

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -548,7 +548,8 @@ m_caster((info->HasAttribute(SPELL_ATTR6_CAST_BY_CHARMER) && caster->GetCharmerO
         // wand case
         if (m_attackType == RANGED_ATTACK)
             if (Item* pItem = playerCaster->GetWeaponForAttack(RANGED_ATTACK))
-                m_spellSchoolMask = SpellSchoolMask(1 << pItem->GetTemplate()->Damage[0].DamageType);
+                if(pItem && pItem->GetTemplate()->SubClass == ITEM_SUBCLASS_WEAPON_WAND)
+                    m_spellSchoolMask = SpellSchoolMask(1 << pItem->GetTemplate()->Damage[0].DamageType);
     }
 
     if (originalCasterGUID)
