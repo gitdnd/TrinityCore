5c7456d96d794a121806825a50caf0fc39ce8fd7
itswicky <blaindes51@gmail.com>
2023-11-09 20:26:40 -0600

    Core/Spell: Check mask, not dispel value, for Fester

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6740,7 +6740,7 @@ float Unit::SpellDamagePctDone(Unit* victim, SpellInfo const* spellProto, Damage
                     Aura const* aura = itr->second->GetBase();
                     SpellInfo const* spell = aura->GetSpellInfo();
 
-                    if (!(spell->GetDispelMask() & DISPEL_DISEASE))
+                    if (!(spell->GetDispelMask() & (1 << DISPEL_DISEASE)))
                         continue;
 
                     totalBonus += bonus * aura->GetStackAmount();
