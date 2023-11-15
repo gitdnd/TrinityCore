3697ad508915882fa8a8b864251c03462740d114
styagrae <50679111+styagrae@users.noreply.github.com>
2023-11-15 19:25:02 +0100

    Update spell_custom_class.cpp
    scaling adjustments for seals again

--- src/server/scripts/Custom/Spells/spell_custom_class.cpp
+++ src/server/scripts/Custom/Spells/spell_custom_class.cpp
@@ -84,7 +84,7 @@ class spell_class_seal_of_righteousness : public AuraScript
             mws /= 1000.0f;
         }
 
-        int32 bp = std::lroundf(mws * (0.05f * ap + 0.045f * sph));
+        int32 bp = std::lroundf(mws * (0.05f * ap + 0.035f * sph));
         CastSpellExtraArgs args(aurEff);
         args.AddSpellBP0(bp);
         GetTarget()->CastSpell(victim, SPELL_CLASS_SEAL_OF_RIGHTEOUSNESS, args);
