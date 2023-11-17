aae198d8e09313ba0bad1eabf0da5954aba490c0
Foereaper <foereaper@elunatech.com>
2023-11-17 23:47:53 +0100

    Fix sending legendaries with prismatic sockets through mail

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -747,7 +747,7 @@ bool Item::CanBeTraded(bool mail, bool trade) const
             return false;
     }
 
-    if (IsBoundByEnchant())
+    if (IsBoundByEnchant() && )
         return false;
 
     return true;
@@ -785,7 +785,7 @@ bool Item::IsBoundByEnchant() const
     for (uint32 enchant_slot = PERM_ENCHANTMENT_SLOT; enchant_slot < MAX_ENCHANTMENT_SLOT; ++enchant_slot)
         if (uint32 enchant_id = GetEnchantmentId(EnchantmentSlot(enchant_slot)))
             if (SpellItemEnchantmentEntry const* enchantEntry = sSpellItemEnchantmentStore.LookupEntry(enchant_id))
-                if (enchantEntry->slot & ENCHANTMENT_CAN_SOULBOUND)
+                if (enchantEntry->slot & ENCHANTMENT_CAN_SOULBOUND && enchant_id != 3729) // Fix for prismatic socket sending
                     return true;
     return false;
 }
