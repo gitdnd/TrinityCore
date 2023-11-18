212e34ac0a33572e9db2b0218bffba273b2e2006
Foereaper <foereaper@elunatech.com>
2023-11-18 02:33:19 +0100

    Potential fix for beacon of light

--- src/server/scripts/Spells/spell_paladin.cpp
+++ src/server/scripts/Spells/spell_paladin.cpp
@@ -1777,8 +1777,6 @@ class spell_pal_light_s_beacon : public SpellScriptLoader
                     return false;
                 if (eventInfo.GetSpellInfo()->Effects->CalcRadius() > 0)
                     return false;
-                if ((eventInfo.GetSpellInfo()->GetSchoolMask() & SPELL_SCHOOL_HOLY) > 0)
-                    return true;
                 return false;
             }
 
