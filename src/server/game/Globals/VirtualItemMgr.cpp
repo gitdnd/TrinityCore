60f2c0cbd7347fc1b307d1c837866b49927d0afb
Tb <tbgitemail@gmail.com>
2023-11-18 17:59:44 -0500

    Make this easier to read.

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -704,7 +704,7 @@ void VirtualItemMgr::GenerateItemStats(VirtualItemTemplate* output, VirtualModif
 
             // hard coded behavior for weapons with spell power.
             if (primarystatgroup[i] == ITEM_MOD_SPELL_POWER)
-                if(output->Class == ITEM_CLASS_WEAPON && !(output->SubClass & ITEM_SUBCLASS_MASK_WEAPON_RANGED))
+                if(output->Class == ITEM_CLASS_WEAPON && output->InventoryType != INVTYPE_RANGED)
                     statPoints *= 4.0f;
 
             if (i < primaryStatSlots && primaryStatSlots > 0)
