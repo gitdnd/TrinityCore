5a47edfdbf6ba50d63660a999e8a120c6bf207ac
Tb <tbgitemail@gmail.com>
2023-11-18 19:23:27 -0500

    E

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -705,8 +705,10 @@ void VirtualItemMgr::GenerateItemStats(VirtualItemTemplate* output, VirtualModif
             // hard coded behavior for weapons with spell power, except ranged weapons
             // one-handed weapons needs a bigger modifier to be balanced to blizz levels of SP
             if (primarystatgroup[i] == ITEM_MOD_SPELL_POWER)
-                if(output->Class == ITEM_CLASS_WEAPON && !((1 << output->SubClass) & ITEM_SUBCLASS_MASK_WEAPON_RANGED))
+            {
+                if (output->Class == ITEM_CLASS_WEAPON && !((1 << output->SubClass) & ITEM_SUBCLASS_MASK_WEAPON_RANGED))
                     statPoints *= 4.0f;
+            }
 
             if (i < primaryStatSlots && primaryStatSlots > 0)
             {
