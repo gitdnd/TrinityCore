c361898d0598f910681abfb0b35ed1ffee4655cc
stoneharry <stoneharryjames@gmail.com>
2024-02-10 02:22:26 +0000

    Fix class order

--- src/server/game/Entities/Creature/AffixMgr.h
+++ src/server/game/Entities/Creature/AffixMgr.h
@@ -7,6 +7,37 @@
 #include <list>
 #include <map>
 
+class AffixItem
+{
+public:
+    AffixItem(uint32 id, uint8 rank);
+
+    uint32 const GetId() { return m_id; };
+    uint8 const GetRank() { return m_rank; }
+
+private:
+    uint32 m_id;
+    uint8 m_rank;
+};
+
+class AffixEffect
+{
+public:
+    AffixEffect(uint32 id, uint32 baseSpell, uint32 targetSpell, uint8 rank);
+
+    uint32 const GetId() { return m_id; }
+    uint32 const GetBaseSpell() { return m_baseSpell; }
+    uint32 const GetTargetSpell() { return m_targetSpell; }
+    uint8 const GetRank() { return m_rank; }
+
+    void Apply(Creature* creature);
+
+private:
+    uint32 m_id;
+    uint32 m_baseSpell;
+    uint32 m_targetSpell;
+    uint8 m_rank;
+};
 
 class AffixMgr
 {
@@ -19,38 +50,6 @@ public:
 private:
 	std::list<AffixItem> m_items;
 	std::list<AffixEffect> m_effects;
-};
-
-class AffixItem
-{
-public:
-	AffixItem(uint32 id, uint8 rank);
-
-	uint32 const GetId() { return m_id; };
-	uint8 const GetRank() { return m_rank; }
-
-private:
-	uint32 m_id;
-	uint8 m_rank;
-};
-
-class AffixEffect
-{
-public:
-	AffixEffect(uint32 id, uint32 baseSpell, uint32 targetSpell, uint8 rank);
-
-	uint32 const GetId() { return m_id; }
-    uint32 const GetBaseSpell() { return m_baseSpell; }
-	uint32 const GetTargetSpell() { return m_targetSpell; }
-	uint8 const GetRank() { return m_rank; }
-
-	void Apply(Creature* creature);
-
-private:
-	uint32 m_id;
-    uint32 m_baseSpell;
-    uint32 m_targetSpell;
-	uint8 m_rank;
 };
 
 #define sAffixMgr AffixMgr::instance()
