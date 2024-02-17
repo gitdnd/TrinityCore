d448a667592e7f2124088b6601c08feebfc17c31
stoneharry <stoneharryjames@gmail.com>
2024-02-17 23:00:15 +0000

    Affix aura script updates

--- src/server/scripts/Custom/Spells/spell_custom_affix.cpp
+++ src/server/scripts/Custom/Spells/spell_custom_affix.cpp
@@ -23,20 +23,23 @@ class spell_affix_avenging_wrath_aura : public AuraScript
 
         std::list<Unit*> list;
 
-        Trinity::AnyFriendlyUnitInObjectRangeCheck checker(caster, caster, range);
-        Trinity::UnitListSearcher<Trinity::AnyFriendlyUnitInObjectRangeCheck> searcher(caster, list, checker);
+        Trinity::AnyUnitInObjectRangeCheck checker(caster, range);
+        Trinity::UnitListSearcher<Trinity::AnyUnitInObjectRangeCheck> searcher(caster, list, checker);
         Cell::VisitAllObjects(caster, searcher, range);
 
         uint32 spellId = GetSpellInfo()->Effects[0].TriggerSpell;
         for (std::list<Unit*>::const_iterator it = list.begin(); it != list.end(); ++it)
         {
-            if ((*it)->HasAura(spellId))
+            if (caster->GetFactionReactionTo((*it)->GetFactionTemplateEntry(), *it) >= REP_NEUTRAL)
             {
-                if (Aura* aura = (*it)->GetAura(spellId))
-                    aura->SetStackAmount(aura->GetStackAmount() + 1);
+                if ((*it)->HasAura(spellId))
+                {
+                    if (Aura* aura = (*it)->GetAura(spellId))
+                        aura->SetStackAmount(aura->GetStackAmount() + 1);
+                }
+                else
+                    caster->CastSpell(*it, spellId);
             }
-            else
-                caster->CastSpell(*it, spellId);
         }
     }
 
@@ -46,7 +49,28 @@ class spell_affix_avenging_wrath_aura : public AuraScript
     }
 };
 
+class spell_affix_arcane_unleashed_aura : public AuraScript
+{
+    PrepareAuraScript(spell_affix_arcane_unleashed_aura);
+
+    void OnProc(AuraEffect const* aurEff, ProcEventInfo& eventInfo)
+    {
+        auto caster = GetCaster();
+        if (!caster || !GetSpellInfo() || !roll_chance_i(GetSpellInfo()->ProcChance))
+        {
+            PreventDefaultAction();
+            return;
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_affix_arcane_unleashed_aura::OnProc, EFFECT_0, SPELL_AURA_PROC_TRIGGER_SPELL);
+    }
+};
+
 void AddSC_Spells_Custom_Affix()
 {
     RegisterAuraScript(spell_affix_avenging_wrath_aura);
+    RegisterAuraScript(spell_affix_arcane_unleashed_aura);
 }
