83da6a9e0e1a066c596bc0087cc23932fafdab13
Tb <tbgitemail@gmail.com>
2024-03-13 16:44:19 -0400

    Update cmake files.

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -3,10 +3,12 @@ target_compile_definitions(trinity-compile-option-interface
   INTERFACE
     -D_BUILD_DIRECTIVE="$<CONFIG>")
 
-set(GCC_EXPECTED_VERSION 7.1.0)
+set(GCC_EXPECTED_VERSION 10.0.0)
 
 if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
   message(FATAL_ERROR "GCC: TrinityCore requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_CXX_COMPILER_VERSION}")
+else()
+  message(STATUS "GCC: Minimum version required is ${GCC_EXPECTED_VERSION}, found ${CMAKE_CXX_COMPILER_VERSION} - ok!")
 endif()
 
 if(PLATFORM EQUAL 32)
@@ -16,7 +18,7 @@ if(PLATFORM EQUAL 32)
       -msse2
       -mfpmath=sse)
 endif()
-if(NOT CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+if(TRINITY_SYSTEM_PROCESSOR MATCHES "x86|amd64")
   target_compile_definitions(trinity-compile-option-interface
     INTERFACE
       -DHAVE_SSE2
@@ -33,11 +35,8 @@ if(WITH_WARNINGS)
       -Winit-self
       -Winvalid-pch
       -Wfatal-errors
-      -Woverloaded-virtual)
-
-  target_compile_options(trinity-warning-interface
-    INTERFACE
-      -Wno-deprecated-copy) # warning in g3d
+      -Woverloaded-virtual
+      -Wno-missing-field-initializers) # this warning is useless when combined with structure members that have default initializers
 
   message(STATUS "GCC: All warnings enabled")
 endif()
--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -1,11 +1,16 @@
 # set up output paths for executable binaries (.exe-files, and .dll-files on DLL-capable platforms)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-set(MSVC_EXPECTED_VERSION 19.24)
-set(MSVC_EXPECTED_VERSION_STRING "Microsoft Visual Studio 2019 16.4")
+set(MSVC_EXPECTED_VERSION 19.32)
+set(MSVC_EXPECTED_VERSION_STRING "Microsoft Visual Studio 2022 17.2")
 
-if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS MSVC_EXPECTED_VERSION)
-  message(FATAL_ERROR "MSVC: TrinityCore requires version ${MSVC_EXPECTED_VERSION} (${MSVC_EXPECTED_VERSION_STRING}) to build but found ${CMAKE_CXX_COMPILER_VERSION}")
+# This file is also used by compilers that pretend to be MSVC but report their own version number - don't version check them
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS MSVC_EXPECTED_VERSION)
+    message(FATAL_ERROR "MSVC: TrinityCore requires version ${MSVC_EXPECTED_VERSION} (${MSVC_EXPECTED_VERSION_STRING}) to build but found ${CMAKE_CXX_COMPILER_VERSION}")
+  else()
+    message(STATUS "MSVC: Minimum version required is ${MSVC_EXPECTED_VERSION}, found ${CMAKE_CXX_COMPILER_VERSION} - ok!")
+  endif()
 endif()
 
 # CMake sets warning flags by default, however we manage it manually
@@ -22,9 +27,9 @@ target_compile_options(trinity-warning-interface
     /W3)
 
 # disable permissive mode to make msvc more eager to reject code that other compilers don't already accept
-#target_compile_options(trinity-compile-option-interface
-#  INTERFACE
-#    /permissive-)
+target_compile_options(trinity-compile-option-interface
+  INTERFACE
+    /permissive-)
 
 # set up output paths ofr static libraries etc (commented out - shown here as an example only)
 #set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
@@ -86,12 +91,9 @@ endif()
 # that the program will eventually be linked with a conforming operator new implementation,
 # and can omit all of these extra null checks from your program.
 # http://blogs.msdn.com/b/vcblog/archive/2015/08/06/new-in-vs-2015-zc-throwingnew.aspx
-if(NOT (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 19.0.23026.0))
-  # makes this flag a requirement to build TC at all
-  target_compile_options(trinity-compile-option-interface
-    INTERFACE
-      /Zc:throwingNew)
-endif()
+target_compile_options(trinity-compile-option-interface
+  INTERFACE
+    /Zc:throwingNew)
 
 # Define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES - eliminates the warning by changing the strcpy call to strcpy_s, which prevents buffer overruns
 target_compile_definitions(trinity-compile-option-interface
@@ -109,7 +111,6 @@ message(STATUS "MSVC: Disabled NON-SECURE warnings")
 target_compile_definitions(trinity-compile-option-interface
   INTERFACE
     -D_CRT_NONSTDC_NO_WARNINGS)
-
 message(STATUS "MSVC: Disabled POSIX warnings")
 
 # Ignore specific warnings
@@ -146,7 +147,8 @@ target_compile_options(trinity-compile-option-interface
     /w34100  # C4100 'identifier' : unreferenced formal parameter
     /w34101  # C4101: 'identifier' : unreferenced local variable
     /w34189  # C4189: 'identifier' : local variable is initialized but not referenced
-    /w34389) # C4189: 'equality-operator' : signed/unsigned mismatch
+    /w34389  # C4389: 'equality-operator' : signed/unsigned mismatch
+	/w35054) # C5054: 'operator 'operator-name': deprecated between enumerations of different types'
 
 # Enable and treat as errors the following warnings to easily detect virtual function signature failures:
 # 'function' : member function does not override any base class virtual member function
@@ -156,20 +158,25 @@ target_compile_options(trinity-compile-option-interface
     /we4263
     /we4264)
 
+if(ASAN)
+  target_compile_definitions(trinity-compile-option-interface
+    INTERFACE
+      -D_DISABLE_STRING_ANNOTATION
+      -D_DISABLE_VECTOR_ANNOTATION)
+
+  target_compile_options(trinity-compile-option-interface
+    INTERFACE
+      /fsanitize=address)
+
+  message(STATUS "MSVC: Enabled Address Sanitizer ASan")
+endif()
+
 # Disable incremental linking in debug builds.
 # To prevent linking getting stuck (which might be fixed in a later VS version).
 macro(DisableIncrementalLinking variable)
   string(REGEX REPLACE "/INCREMENTAL *" "" ${variable} "${${variable}}")
   set(${variable} "${${variable}} /INCREMENTAL:NO")
 endmacro()
-
-# Disable Visual Studio 2022 build process management
-# This will make compiler behave like in 2019 - compiling num_cpus * num_projects at the same time
-# it is neccessary because of a bug in current implementation that makes scripts build only a single
-# file at the same time after game project finishes building
-if (NOT MSVC_TOOLSET_VERSION LESS 143)
-  file(COPY "${CMAKE_CURRENT_LIST_DIR}/Directory.Build.props" DESTINATION "${CMAKE_BINARY_DIR}")
-endif()
 
 DisableIncrementalLinking(CMAKE_EXE_LINKER_FLAGS_DEBUG)
 DisableIncrementalLinking(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO)
