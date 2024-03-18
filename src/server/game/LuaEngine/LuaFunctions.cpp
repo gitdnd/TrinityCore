a98d1c4016b78753e30af5766024d48c2463e137
Tb <tbgitemail@gmail.com>
2024-03-18 17:54:06 -0400

    Add call to register custom functions.

--- src/server/game/LuaEngine/LuaFunctions.cpp
+++ src/server/game/LuaEngine/LuaFunctions.cpp
@@ -36,6 +36,7 @@ extern "C"
 #include "CorpseMethods.h"
 #include "VehicleMethods.h"
 #include "BattleGroundMethods.h"
+#include "CustomMethods.h"
 
 // Template by Mud from http://stackoverflow.com/questions/4484437/lua-integer-type/4485511#4485511
 template<> int ElunaTemplate<unsigned long long>::Add(lua_State* L) { Eluna* E = Eluna::GetEluna(L); E->Push(E->CHECKVAL<unsigned long long>(1) + E->CHECKVAL<unsigned long long>(2)); return 1; }
@@ -185,4 +186,6 @@ void RegisterFunctions(Eluna* E)
     ElunaTemplate<ObjectGuid>::Register(E, "ObjectGuid");
 
     LuaVal::Register(E->L);
+
+    LuaCustom::RegisterCustomFunctions(E);
 }
