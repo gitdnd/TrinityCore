98086e29f23108638767b32d5e0b972aeb989bff
Tb <tbgitemail@gmail.com>
2024-04-10 16:55:01 -0400

    Update debug message.

--- src/server/game/LuaEngine/ElunaLoader.cpp
+++ src/server/game/LuaEngine/ElunaLoader.cpp
@@ -51,9 +51,9 @@ static ElunaUpdateListener elunaUpdateListener;
 
 void ElunaUpdateListener::handleFileAction(efsw::WatchID, std::string const& dir, std::string const& filename, efsw::Action action, std::string oldFilename)
 {
-    ELUNA_LOG_INFO("[Eluna]: Found file %s change.", filename.c_str());
-
     auto const path = fs::absolute(filename, dir);
+    ELUNA_LOG_INFO("[Eluna]: Found file %s change.", path.c_str());
+
     if (!path.has_extension())
         return;
 
