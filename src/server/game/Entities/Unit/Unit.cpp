a7c55099079269f8d196f6ad12d42e344c01d215
itswicky <blaindes51@gmail.com>
2024-05-30 22:41:53 -0500

    Core/Spell: Fix Lava Burst crit interaction

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7400,7 +7400,6 @@ float Unit::SpellCritChanceTaken(Unit const* caster, SpellInfo const* spellInfo,
                         // Shiv-applied poisons can't crit
                         if (caster->FindCurrentSpellBySpellId(5938))
                             crit_chance = 0.0f;
-
                         // Exorcism
                         else if (spellInfo->GetCategory() == 19)
                         {
@@ -7408,9 +7407,8 @@ float Unit::SpellCritChanceTaken(Unit const* caster, SpellInfo const* spellInfo,
                                 return 100.0f;
                             break;
                         }
-
                         // Lava Burst
-                        else if (spellInfo->SpellFamilyFlags[1] & 0x00001000)
+                        else if (caster->FindCurrentSpellBySpellId(60043))
                         {
                             if (GetAura(49233, caster->GetGUID()))
                                 if (GetTotalAuraModifier(SPELL_AURA_MOD_ATTACKER_SPELL_AND_WEAPON_CRIT_CHANCE) > -100)
