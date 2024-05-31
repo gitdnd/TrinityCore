45dececd9614bff856a77351a1ee0e098de4198f
Tb <tbgitemail@gmail.com>
2024-05-31 16:20:35 -0400

    Update string formating for honing break announcement.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -6021,12 +6021,8 @@ void Spell::EffectHoneVirtualItem()
         player->PlayDirectSound(13092); // Item break sound
         if (itemTarget->IsBroken())
         {
-            std::ostringstream oss;
-            oss << player->GetName() << " has broken their " <<
-                "|c" << std::hex << ItemQualityColors[ITEM_QUALITY_LEGENDARY] << std::dec <<
-                "|Hitem:" << itemTarget->GetEntry() << ":0:" <<
-                "0:0:0:0:" << "0:0:0:0|h[" << itemTarget->GetTemplate()->Name1 << "]|h|r" << "!";
-            sWorld->SendGlobalText(oss.str().c_str(), nullptr, CHAT_MSG_LOOT);
+            sWorld->SendGlobalText(Trinity::StringFormat("{} has broken their |c{}{}{}|Hitem:{}:0:0:0:0:0:0:0:0:0|h[{}]|h|r!", player->GetName(), std::hex, ItemQualityColors[ITEM_QUALITY_LEGENDARY],
+                std::dec, itemTarget->GetEntry(), itemTarget->GetTemplate()->Name1).c_str(), nullptr, CHAT_MSG_LOOT);
         }
         return;
     }
