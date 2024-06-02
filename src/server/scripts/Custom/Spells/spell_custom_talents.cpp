92ab1d7e278fd0555d9531a20ba73b06f586e708
itswicky <blaindes51@gmail.com>
2024-06-01 21:49:37 -0500

    Core/Spell: Adjust some caclulation logic

--- src/server/scripts/Custom/Spells/spell_custom_talents.cpp
+++ src/server/scripts/Custom/Spells/spell_custom_talents.cpp
@@ -2717,9 +2717,9 @@ class spell_hot_lightning_bolt : public AuraScript
                 SpellInfo const* lightningboltPassive = sSpellMgr->AssertSpellInfo(160308);
                 int32 rollChance = lightningboltPassive->GetEffect(EFFECT_1).CalcValue(player);
                 player->GetSession()->SendNotification("Base success chance: %i", rollChance);
-                float castMod = player->GetFloatValue(UNIT_MOD_CAST_SPEED);
+                float castMod = (1 / player->GetFloatValue(UNIT_MOD_CAST_SPEED));
                 player->GetSession()->SendNotification("Current Cast Speed Mod: %f", castMod);
-                rollChance += ((int32(castMod) - 1) / 2) * 100;
+                rollChance += int32(((castMod - 1) / 2) * 100);
                 player->GetSession()->SendNotification("Current success chance: %i", rollChance);
                 
                 return roll_chance_i(rollChance);
