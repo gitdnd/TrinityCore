697ae45d56550752d964f72baf6ea73231398b23
Tb <tbgitemail@gmail.com>
2024-06-19 12:35:36 -0400

    fix missed case from previous commit.

--- src/server/game/Handlers/LootHandler.cpp
+++ src/server/game/Handlers/LootHandler.cpp
@@ -94,6 +94,13 @@ void WorldSession::HandleAutostoreLootItemOpcode(WorldPacket& recvData)
         loot = &creature->loot;
     }
 
+    lootSlot = loot->lootSlotFromIndex(lootSlot, player);
+    if (lootSlot == -1)
+    {
+        TC_LOG_DEBUG("loot", "lootSlotFromIndex returned -1 for player {}", GetPlayer()->GetName().c_str());
+        return;
+    }
+
     player->StoreLootItem(lootSlot, loot);
 
     // If player is removing the last LootItem, delete the empty container.
