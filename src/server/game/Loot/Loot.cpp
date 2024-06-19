bf2a3f376c7a41984948b08df5373007de704856
Tb <tbgitemail@gmail.com>
2024-06-19 12:11:11 -0400

    More loot testing.

--- src/server/game/Loot/Loot.cpp
+++ src/server/game/Loot/Loot.cpp
@@ -61,9 +61,9 @@ LootItem::LootItem(LootStoreItem const& li)
     rollWinnerGUID = ObjectGuid::Empty;
 }
 
-bool LootItem::AllowedForPlayer(Player const* player, ObjectGuid /*ownerGuid*/) const
+bool LootItem::AllowedForPlayer(Player const* player, ObjectGuid ownerGuid) const
 {
-    return AllowedForPlayer(player, false, ObjectGuid::Empty);
+    return AllowedForPlayer(player, false);
 }
 
 bool LootItem::AllowedForPlayer(Player const* player, bool isGivenByMasterLooter) const
