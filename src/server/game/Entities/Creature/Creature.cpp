81b93f80b20a412c25ed4692090f6cdf02bdfd39
Tb <tbgitemail@gmail.com>
2024-06-20 16:48:40 -0400

    Fix loading creatures crashing the server due to trying to get the creature's entry too early.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -274,10 +274,6 @@ Creature::Creature(bool isWorldObject): Unit(isWorldObject), MapObject(), m_grou
     ResetLootMode(); // restore default loot mode
     m_isTempWorldObject = false;
     _dungeonLevelOverride = 0;
-    if (auto cT = sObjectMgr->GetCreatureTemplate(GetEntry()))
-    {
-        flagsExtraOverride = cT->flags_extra;
-    }
 }
 
 void Creature::AddToWorld()
@@ -299,6 +295,11 @@ void Creature::AddToWorld()
 
         if (GetZoneScript())
             GetZoneScript()->OnCreatureCreate(this);
+
+        if (auto cT = sObjectMgr->GetCreatureTemplate(GetEntry()))
+        {
+            flagsExtraOverride = cT->flags_extra;
+        }
 
         ApplyAffixData(AFFIX_EVENT_ADD_TO_WORLD);
 
