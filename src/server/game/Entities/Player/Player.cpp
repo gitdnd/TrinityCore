1f5a1a73bd5b82db039cb4e2514dc94d58290905
stoneharry <stoneharryjames@gmail.com>
2024-06-20 12:18:13 +0100

    Add debug

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -28152,15 +28152,23 @@ void Player::LearnCustomTalent(uint32 id)
     customTalents[GetCurrentTalentLoadout()].push_back(id);
 
     if (Aura* aura = GetAura(nodeInfo->spellId, GetGUID()))
-        if(aura->GetSpellInfo()->StackAmount > 1)
+    {
+        this->Say("Updating stack amount!", LANG_UNIVERSAL);
+
+        //if (aura->GetSpellInfo()->StackAmount > 1)
             aura->SetStackAmount(GetTalentStackCount(nodeInfo->spellId));
+    }
     else
     {
         //LearnSpell(nodeInfo->spellId, false);
         AddTemporarySpell(nodeInfo->spellId);
         if (const SpellInfo* spellInfo = sSpellMgr->GetSpellInfo(nodeInfo->spellId))
+        {
+            this->Say("Casting spell!", LANG_UNIVERSAL);
+
             //if (spellInfo->IsPassive() && HandlePassiveSpellLearn(spellInfo))
                 CastSpell(this, nodeInfo->spellId, true);
+        }
     }
 
  
