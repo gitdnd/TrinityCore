3c954e942a4191f0d7f836ec785af6eff0076171
Foereaper <foereaper@elunatech.com>
2024-06-24 23:16:13 +0200

    Add base dodge rating

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -825,7 +825,7 @@ float const dodge_cap[MAX_CLASSES] =
 
 void Player::UpdateDodgePercentage()
 {
-    float diminishing = 0.0f, nondiminishing = 0.0f;
+    float diminishing = 0.0f, nondiminishing = 5.0f;
     // GetDodgeFromAgility(diminishing, nondiminishing);
     // Modify value from defense skill (only bonus from defense rating diminishes)
     nondiminishing += (int32(GetSkillValue(SKILL_DEFENSE)) - int32(GetMaxSkillValueForLevel())) * 0.04f;
