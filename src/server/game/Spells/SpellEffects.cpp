2690cd73a29d4070d66e8fff5d7776c943b14de4
tb <tbgitemail@gmail.com>
2024-11-03 19:28:25 -0500

    Allow SPELL_EFFECT_POWER_BURN to target focus.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1296,7 +1296,7 @@ void Spell::EffectPowerBurn()
         return;
 
     Powers powerType = Powers(effectInfo->MiscValue);
-    if (!unitTarget || !unitTarget->IsAlive() || unitTarget->GetPowerType() != powerType || damage < 0)
+    if (!unitTarget || !unitTarget->IsAlive() || unitTarget->GetPowerType() != powerType && powerType != POWER_FOCUS || damage < 0)
         return;
 
     Unit* unitCaster = GetUnitCasterForEffectHandlers();
