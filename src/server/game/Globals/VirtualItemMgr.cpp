28572d396c67e7fda0f3204a759d48d376fcd64d
stoneharry <stoneharryjames@gmail.com>
2024-12-22 00:34:11 +0000

    Slight tweaks to item level generation

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -417,7 +417,7 @@ void VirtualItemMgr::GenerateItemLevel(VirtualItemTemplate* output, VirtualModif
     int32 vLevel = GetVirtualLevel(float(modifier.plrAvgLvl));
 
     // Mod the virtual item level to allow higher or lower virtual item levels 
-    vLevel += irand(-1, 4, generator);
+    vLevel += irand(-1, 3, generator);
 
     // Add all vLvl mods before generating a new ilevel
     vLevel += int32(modifier.vLvlMod);
@@ -426,7 +426,7 @@ void VirtualItemMgr::GenerateItemLevel(VirtualItemTemplate* output, VirtualModif
     ilevel = uint32(round(GenerateItemLevel(vLevel)));
 
     // Modify the returned, newly generated iLevel based on quality
-    ilevel += (output->Quality * 2);
+    ilevel += round(output->Quality * 1.5f);
 
     // One last mod to the ilevel to try to smooth out any ilevel groups and spikes
     ilevel += irand(-3, 2, generator);
