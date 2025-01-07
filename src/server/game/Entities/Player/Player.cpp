4578207eaf1039eea102626e0ab9530df09a62a5
tb <tbgitemail@gmail.com>
2025-01-07 13:27:38 -0500

    Exempt staff from instance saves by default.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19613,6 +19613,9 @@ void Player::UnbindInstance(BoundInstancesMap::iterator &itr, Difficulty difficu
 
 InstancePlayerBind* Player::BindToInstance(InstanceSave* save, bool permanent, BindExtensionState extendState, bool load)
 {
+    if (GetSession()->GetSecurity() >= SEC_MODERATOR)
+        return nullptr;
+
     if (save)
     {
         InstancePlayerBind& bind = m_boundInstances[save->GetDifficulty()][save->GetMapId()];
@@ -19679,6 +19682,9 @@ InstancePlayerBind* Player::BindToInstance(InstanceSave* save, bool permanent, B
 
 void Player::BindToInstance()
 {
+    if (GetSession()->GetSecurity() >= SEC_MODERATOR)
+        return;
+
     InstanceSave* mapSave = sInstanceSaveMgr->GetInstanceSave(_pendingBindId);
     if (!mapSave) //it seems sometimes mapSave is nullptr, but I did not check why
         return;
@@ -19695,6 +19701,9 @@ void Player::BindToInstance()
 
 void Player::SetPendingBind(uint32 instanceId, uint32 bindTimer)
 {
+    if (GetSession()->GetSecurity() >= SEC_MODERATOR)
+        return;
+
     _pendingBindId = instanceId;
     _pendingBindTimer = bindTimer;
 }
