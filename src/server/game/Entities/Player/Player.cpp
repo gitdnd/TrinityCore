d65577aa8028d9b33ed3022fc99079c41fd4e71a
tb <tbgitemail@gmail.com>
2025-01-10 16:56:23 -0500

    W h y
    c
    a
    n
    t
    
    f
    o
    r
    m
    a
    t
    i
    n
    g
    
    b
    e
    
    c
    o
    n
    s
    i
    s
    t
    e
    n
    t
    
    f
    o
    r
    
    p
    a
    r
    s
    i
    n
    g
    
    s
    t
    r
    i
    n
    g
    
    v
    a
    r
    i
    a
    b
    l
    e
    s

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2667,7 +2667,7 @@ void Player::InitTalentForLevel()
     uint32 level = GetXPTalentLevel();
     uint32 bonusLevel = GetBonusTalentLevel();
     int32 newTalentPoints = (level + bonusLevel) - GetUsedTalentCount();
-    ChatHandler(GetSession()).PSendSysMessage("Debug: level {}, bonus level {}, used points {}, new points {}", level, bonusLevel, GetUsedTalentCount(), newTalentPoints);
+    ChatHandler(GetSession()).PSendSysMessage("Debug: level %u, bonus level %u, used points %u, new points %i", level, bonusLevel, GetUsedTalentCount(), newTalentPoints);
     if (newTalentPoints < 0)
     {
         newTalentPoints = 0;
