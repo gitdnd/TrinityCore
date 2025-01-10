a3f27aa9e217bde6b3172056cac7ebe5b23287e1
stoneharry <stoneharryjames@gmail.com>
2025-01-10 23:28:02 +0000

    Add debug

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -279,6 +279,8 @@ bool Item::Create(ObjectGuid::LowType guidlow, uint32 itemId, Player const* owne
     // VirtualItem
     if (VirtualItemMgr::IsVirtualTemplate(itemProto))
     {
+        owner->Say("modifier level, owner level: " + modifier.plrAvgLvl + ", " + owner->GetCappedItemLevel(), LANG_UNIVERSAL);
+
         // pass the players' average item level to the item generator
         if(modifier.plrAvgLvl == 0)
             modifier.plrAvgLvl = uint32(owner->GetCappedItemLevel());
