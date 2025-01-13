c4d24b559d30ee3fc8b0079e97a4b56dc0c848c0
tb <tbgitemail@gmail.com>
2025-01-13 16:21:23 -0500

    Fix missing functions for transports.

--- src/server/game/LuaEngine/methods/TrinityCore/CustomMethods.h
+++ src/server/game/LuaEngine/methods/TrinityCore/CustomMethods.h
@@ -2128,6 +2128,9 @@ namespace LuaCustom
         ElunaTemplate<Transport>::SetMethods(E, TransportMethods);
         ElunaTemplate<Transport>::SetMethods(E, ObjectMethods);
         ElunaTemplate<Transport>::SetMethods(E, WorldObjectMethods);
+        ElunaTemplate<Transport>::SetMethods(E, LuaObject::ObjectMethods);
+        ElunaTemplate<Transport>::SetMethods(E, LuaWorldObject::WorldObjectMethods);
+        ElunaTemplate<Transport>::SetMethods(E, LuaGameObject::GameObjectMethods);
     };
 };
     
