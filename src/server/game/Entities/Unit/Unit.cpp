45d504dab8c5671d9f8864bb26e6f7a606c93f64
kaleid <50679111+styagrae@users.noreply.github.com>
2025-01-26 16:23:59 +0100

    remove focus gain on melee attack

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13516,9 +13516,10 @@ void Unit::RewardRage(uint32 damage, uint32 weaponSpeedHitFactor, bool attacker)
 
     if (GetTypeId() != TYPEID_PLAYER)
         ModifyPower(POWER_RAGE, uint32(addRage * 10));
-
+    /*
     if (GetTypeId() == TYPEID_PLAYER)
         ModifyPower(POWER_FOCUS, uint32(addRage / 2));
+        */
 }
 
 void Unit::StopAttackFaction(uint32 faction_id)
