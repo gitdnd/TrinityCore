c90df78c1cbe5536737c75203f46f2e3bb05f49c
tb <tbgitemail@gmail.com>
2025-02-04 11:41:55 -0500

    Updates to how stat group mask are checked.

--- src/server/game/Globals/VirtualItemMgr.cpp
+++ src/server/game/Globals/VirtualItemMgr.cpp
@@ -2139,10 +2139,10 @@ void VirtualItemMgr::GenerateLegendaryItemEffect(VirtualItemTemplate* output, Vi
         if (SelectSkipDebug(itr.second.itemStatGroup, output->statGroup, "[StatGroup]"))
             continue;
 
-        if(itr.second.itemStatGroupMask && !(itr.second.itemStatGroupMask & StatGroupToMask(output->statGroup)))
+        if(itr.second.itemStatGroupMask && output->HasStatGroupMask(StatGroupGroup(itr.second.itemStatGroupMask)))
             continue;
 
-        if (itr.second.legendaryFlags && (itr.second.legendaryFlags & LEGENDARY_FLAG_CRAFTED_ONLY) && !modifier.isCrafted)
+        if (itr.second.legendaryFlags && (itr.second.legendaryFlags & LEGENDARY_FLAG_CRAFTED_ONLY) != 0 && !modifier.isCrafted)
             continue;
 
         bool skip = false;
