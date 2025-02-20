3ef5b7e8a32ef9558a39d2dc44132fcbd1a05eb0
kaleid <50679111+styagrae@users.noreply.github.com>
2025-02-20 16:17:47 +0100

    Update stam to hp formula

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -307,11 +307,12 @@ void Player::UpdateArmor()
 
 float Player::GetHealthBonusFromStamina()
 {
-    float stamina = GetStat(STAT_STAMINA);
-    float baseStam = std::min(20.0f, stamina);
-    float moreStam = stamina - baseStam;
+    //float stamina = GetStat(STAT_STAMINA);
+    //float baseStam = std::min(20.0f, stamina);
+    //float moreStam = stamina - baseStam;
 
-    return baseStam + (moreStam*10.0f);
+    //return baseStam + (moreStam*10.0f);
+    return GetStat(STAT_STAMINA) * 10.f;
 }
 
 float Player::GetManaBonusFromIntellect()
